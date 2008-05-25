36d54639d18549b0254ef4e6dd1ec0719c829d4b
Jun Li <jli127@cs.mcgill.ca>
2008-05-25 13:39:56 +0000

    Grammar update, no AST
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@102 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@890b462f6fbdbb5188a238f6f0df0140058921e1

--- languages/matrix/Test.m
+++ languages/matrix/Test.m
@@ -1,26 +1,70 @@
-# this is single line comment
-% this is another % single line comment
-% java -classpath .;../../../lib/beaver-rt.jar Main Test.m >log.txt
-		a = 5;
-		matrixA = [1, 2; 3, a=5; a, a+6];
-		matrixA = [1, 2, 3, a=5];
+# current a program consists of a list of statements, which separated by ;
+# except single line comment 
+# Notes 
+#  - Current grammar doesn't support 
+#		- cell, structure, object, ...
+#  		- Left-Div series operators
+#  		- list of lhs variables, i.g.  [a, b]=foo();
+#  - Another way to run the test : java Main Test.m >log.txt
 
-		matrixC = 1:9;
-		matrixD = 1:a+7;
-		matrixC(1:5) = a+6;		
-		
-		matrixX = [];
-		matrixY = [;];
-		matrixZ = [1,];
-		matrixA = [1, 2; 3, a=5; [a, a+6]];
-		Mb = matrixA;
-		Mc = matrixA(1);
-		Mc = matrixA(1,:);
-		Mc = matrixA();		% RHS, OK, matrix_access ==  method_invocation
+# following are some test cases
+;
 
-		matrixA(1, 1) = a;
-		matrixA(:) = [a, 9];
-		matrixA(1, :) = a;
+# some operators
+x = a**5;
+y = b^6;
 
-%		 matrixA() = a;		% LHS, report error
-		
+x ^= 6;
+y **= 7;
+
+a > b;
+a < b;
+c == d;
+e <= f;
+g >= k;
+l != m;
+n ~= q;
+
+!a == b;
+c != !d;
+e ~= ~f;
+
+a .+= b;
+a ./= b;
+c .**= d;
+c .^= d;
+	
+a='string';
+b="DOUBLUE QU";
+a = 5;
+
+# Matrix...
+matrixA = [1, 2; 3, a=5; a, a+6];
+matrixA = [1, 2, 3, a=5];
+
+matrixC = 1:9;
+matrixD = 1:a+7;
+matrixC(1:5) = a+6;		
+
+matrixX = [];
+matrixY = [;];
+matrixZ = [1,];
+matrixA = [1, 2; 3, a=5; [a, a+6]];
+Mb = matrixA;
+Mc = matrixA(1);
+Mc = matrixA(1,:);
+Mc = matrixA();		% RHS, OK, matrix_access ==  method_invocation
+
+matrixA(1, 1) = a;
+matrixA(:) = [a, 9];
+matrixA(1, :) = a;
+
+%	 matrixA() = a;		% LHS, report error
+
+# the precedence of this kind of statements need check more
+a = a=5;
+
+# Currently, the grammar doesn't support Left-Div series operators
+% a\b
+% c.\d
+% e .\= f
