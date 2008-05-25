6df0123a665d5c6bf1cb0b1a9f828964ca17c5c1
Andrew Casey <acasey@cs.mcgill.ca>
2008-05-25 00:16:02 +0000

    Tool for generating .out files for scanner tests.
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@87 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@26c841751a978b63bb47a0a0a4332a056e207380

---
+++ languages/Natlab/test/natlab/ScannerTestTool.java
@@ -0,0 +1,51 @@
+package natlab;
+
+import java.io.*;
+
+import beaver.Scanner;
+import beaver.Symbol;
+
+/**
+ * A utility for producing the output file corresponding to a given input file.
+ * Note that the output should be checked manually before using it as a test.
+ */
+public class ScannerTestTool {
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("Usage: java natlab.ScannerTestTool {basename}");
+			System.exit(1);
+		}
+		String basename = args[0];
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(basename + ".in"));
+			Scanner scanner = new NatlabScanner(in);
+			PrintWriter out = new PrintWriter(new FileWriter(basename + ".out"));
+			while(true) {
+				Symbol curr = scanner.nextToken();
+				if(curr.getId() == NatlabParser.Terminals.EOF) {
+					break;
+				}
+				if(curr.value != null) {
+					out.print(curr.value);
+					out.print(' ');
+				}
+				out.print(NatlabParser.Terminals.NAMES[curr.getId()]);
+				out.print(' ');
+				int start = curr.getStart();
+				int startLine = Symbol.getLine(start);
+				int startCol = Symbol.getColumn(start);
+				out.print(startLine);
+				out.print(' ');
+				out.print(startCol);
+				out.print(' ');
+				out.println(curr.getEnd() - start + 1);
+			}
+			out.close();
+			in.close();
+			System.exit(0);
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.exit(2);
+		}
+	}
+}
