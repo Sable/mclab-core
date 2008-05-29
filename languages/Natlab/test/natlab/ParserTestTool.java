4664cf32597a3b993ae0a692fc06273b0b8a276f
Andrew Casey <acasey@cs.mcgill.ca>
2008-05-29 14:39:16 +0000

    Tool for generating parser .out files.
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@165 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@bfa1f70596090f7a2dd5f5ea25dfc8035725540c

---
+++ languages/Natlab/test/natlab/ParserTestTool.java
@@ -0,0 +1,48 @@
+package natlab;
+
+import java.io.*;
+
+import natlab.ast.Root;
+import beaver.Parser;
+import beaver.Scanner;
+
+/**
+ * A utility for producing the output file corresponding to a given input file.
+ * Note that the output should be checked manually before using it as a test.
+ */
+public class ParserTestTool {
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("Usage: java natlab.ParserTestTool {basename}");
+			System.exit(1);
+		}
+		String basename = args[0];
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(basename + ".in"));
+			Scanner scanner = new NatlabScanner(in);
+			NatlabParser parser = new NatlabParser();
+			Root actual = (Root) parser.parse(scanner);
+			PrintWriter out = new PrintWriter(new FileWriter(basename + ".out"));
+			if(parser.hasError()) {
+				for(String error : parser.getErrors()) {
+					out.println(error);
+				}
+			} else {
+				int startPos = actual.getStart();
+				int endPos = actual.getEnd();
+				out.println(Root.getLine(startPos) + " " + Root.getColumn(startPos));
+				out.println(Root.getLine(endPos) + " " + Root.getColumn(endPos));
+				out.println(actual.getStructureString());
+			}
+			out.close();
+			in.close();
+			System.exit(0);
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.exit(2);
+		} catch (Parser.Exception e) {
+			e.printStackTrace();
+			System.exit(3);
+		}
+	}
+}
