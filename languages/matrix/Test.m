ee8375c45c1af0f8e882a0a7732bf83b65e17ba3
Jun Li <jli127@cs.mcgill.ca>
2008-06-04 23:52:31 +0000

    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@269 89b902b4-d83f-0410-b942-89f7a419ffdb
    Migrated from Sable/mclab@9be2169aecd8f44dab396e2e93b3ebcf13c61f34

--- languages/matrix/Test.m
+++ languages/matrix/Test.m
@@ -9,15 +9,210 @@
 %     - java Main Test0.m >log.txt
 
 % following are some test cases
+%================================
+% Testing about switch
+%================================
+switch a
+end
+
+switch a
+otherwise
+end
+
+switch a
+case 1
+otherwise
+end
+
+switch a
+case 1
+  a = a * 2 
+otherwise
+  a = a - 1
+end
+
+switch a
+case 1
+  a = a * 2 
+case 2
+  a = a / 2 
+case 3
+  a = a ^ 2 
+otherwise
+  a = a - 1
+end
+
 
 %================================
-% ERROR : No annoteted comments
+% Testing about try
+%================================
+try
+  A = B/C
+catch
+  A = B
+end
+
+try
+catch
+end
+
+
+if n < 0 % If n negative, display error message.
+	disp('Input must be positive');
+elseif rem(n,2) == 0 % If n positive and even, divide by 2.
+	A = n/2;
+else
+	A = (n+1)/2; % If n positive and odd, increment and divide.
+end
+
+%================================
+% Testing about the stmt_sep 
+% if / else / elseif / 
 %================================
 
+n = 10; if n < 0; A=1; else
+ A = (n+1)/2, end
+
+%-------------------------------
+n = 10;  
+if  n < 0  % No stmt_sep alow just after if
+   A = 11
+elseif n > 2
+   A = (n+1)/2 
+else 
+   A = (n+1) 
+end
+%-------------------------------
+n = 10;  
+if  n < 0   
+   A = -n/2,
+end
+%-------------------------------
+if  n < 0   
+   A = -n/2,
+else
+   A = (n+1)/2, 
+end
+%-------------------------------
+if  n < 0   
+   A = -n/2,
+elseif n > 2
+   A = (n+1)/2, 
+end
+%-------------------------------
+n = 10;  
+if  n < 0   
+   A = -n/2,
+elseif n > 10
+   A = (n+1)/2, 
+elseif n > 6
+   A = (n+1)/2+1, 
+elseif n > 2
+   A = (n)/2+1, 
+end
+%-------------------------------
+n = 10;  
+if  n < 0   
+   A = -n/2,
+elseif n > 10
+   A = (n+1)/2, 
+elseif n > 6
+   A = (n+1)/2+1, 
+elseif n > 2
+   A = (n)/2+1, 
+else
+   A = n,
+end
+%-------------------------------
+
+n = 10; if n < 0 else A = (n+1)/2; end
+
+if  n < 0  % No stmt_sep alow just after if
+else 
+   A = (n+1)/2; 
+end
+
+%===========================================
+% Error: cannot handle cases
+% other stmt_sep, treated as Empty statement
+n = 10; if n < 0 else ; A = (n+1)/2; end
+n = 10; if n < 0; A=1; else A = (n+1)/2, end
+
+% need stmt_sep after else!!
+n = 10; if n < 0; A=1; else; A = (n+1)/2, end
+
+n = 10; if n < 0; A=1; else
+ A = (n+1)/2, end
+
+% Tranditional form
+n = 10; 
+if n < 0
+   A=1 
+else 
+   A = (n+1)/2
+end
+
+n = 10; if n < 0 A=1; else A = (n+1)/2; end
+
+%================================
+% Testing about the stmt_sep  after end
+x=1; for j=1:5   x = x + j; end         % need all of stmt_sep ';'
+x=1; for j=1:5   x = x + j, end, y=10   % need stmt_sep after end
+
+[1, 18]  unexpected token IDENTIFIER
+[1, 14]  try inserting LINE_TERMINATOR
+
+
+% === CANNOT SUPPORT !!! ======
+% do not need stmt_sep after
+  FOR for_assign.a stmt_separator? stmt_list.l END
+
+% support stmt_sep !!!
+x=1; for j=1:5,   x = x + j, end, y=10
+x=1; for j=1:5,
+   x = x + j, end
+x=1; for j=1:5
+   x = x + j, end
+
+
+{: return new ElseifStmt(new List<ElseifStmt>(), l); :}
+/*
+//-----------------------------------------
+// Another way for IF--ELSE
+
+if_statement = 
+     IF if_stmt_list END
+  |  IF if_stmt_list else_clause END
+  ;
+
+if_stmt_list = 
+     expr.e stmt_separator stmt_list.l {: return new IfStmt(e, l); :}
+  |  if_stmt_list elseif_clause
+  ;
+
+elseif_clause = 
+     ELSEIF expr.e stmt_separator stmt_list.l {: return new IfStmt(e, l); :}
+  ;
+
+else_clause = 
+     ELSE stmt_separator? stmt_list.l
+  ;
+*/
+
+%================================
 x = 0; n = 1;
 % comment-for
 for a=1:3;  % comment-assign
 x = x+1; break;  n = n*2, % comment-stmt 
+end
+
+% comment-for
+for a=1:3  % comment-assign
+  x = x+1, 
+  n = n*2, % comment-stmt 
+  if n>10
+    break;
+  end 
 end
 
 x = 0; n = 1;
@@ -26,6 +221,9 @@ while ~feof(fid)  % comment-expr
 x = x+1; continue;  n = n*2, % comment-stmt 
 end
 
+while ~feof(fid)  
+   x = x+fid
+end
 
 
 x = 0; n = 1;
@@ -43,13 +241,14 @@ end
 % Accept statement_separator =  SEMICOLON? LINE_TERMINATOR
 x=0;n=1;y=1:10;b=1:3;
 for i=b
-  x = x+1;
+  x = x+i;
 	for j=1:5
-	  x = x + y(j)
+	  x = x * j
 	end
-  n = n*2;
+  n = n+x^2
 end
 
+for a=1:3  x = x+1;   n = n*2; end
 for a=1:3;  x = x+1;   n = n*2; end
 
 for j = (1 : 5)
