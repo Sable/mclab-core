f6e4113d201d545fff4780bbcf01426e1804c323
Andrew Casey <acasey@cs.mcgill.ca>
2008-06-20 03:49:03 +0000

    Simple translator program.  Compiles but doesn't work.
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@366 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@2639bf6092e3a72c6c0e5fe82281c354daac1150

---
+++ languages/Matlab/src/matlab/Translator.java
@@ -0,0 +1,45 @@
+package matlab;
+
+import java.io.*;
+
+import matlab.ast.Program;
+import beaver.Parser;
+
+/**
+ * A utility for translating from Matlab source to Natlab source.
+ */
+public class Translator {
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("Usage: java matlab.Translator {basename}");
+			System.exit(1);
+		}
+		String basename = args[0];
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(basename + ".m"));
+			
+			ExtractionScanner scanner = new ExtractionScanner(in);
+			ExtractionParser parser = new ExtractionParser();
+			Program actual = (Program) parser.parse(scanner);
+			PrintWriter out = new PrintWriter(new FileWriter(basename + ".n"));
+			if(parser.hasError()) {
+				for(String error : parser.getErrors()) {
+					System.err.println(error);
+				}
+			} else {
+				out.print(actual.translate());
+			}
+			out.close();
+			in.close();
+			System.exit(0);
+		} catch(TranslationException e) {
+		    System.err.println("[" + e.getLine() + ", " + e.getCol() + "]  " + e.getMessage());
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.exit(2);
+		} catch (Parser.Exception e) {
+			e.printStackTrace();
+			System.exit(3);
+		}
+	}
+}
