7d4cde8328348b789d310a1dd5f269afc9da0732
Jesse Doherty <jdoher1@cs.mcgill.ca>
2008-07-03 20:04:33 +0000

    Add new constructors and field for proper function scope hierarchy
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@502 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@5fe0261314951a72ff3f4b3093c79fe61282fe68

--- languages/Natlab/src/natlab/SymbolTableFunctionEntry.java
+++ languages/Natlab/src/natlab/SymbolTableFunctionEntry.java
@@ -8,6 +8,7 @@ public class SymbolTableFunctionEntry
     private SymbolTableScope localSymbols;
     private HashMap< String, SymbolTableFunctionEntry > nestedFunctions;
     private HashMap< String, SymbolTableFunctionEntry > localFunctions;
+    private HashMap< String, SymbolTableFunctionEntry > parentLocals;
 
     //TODO-JD: Add some input output type info
 
@@ -17,6 +18,7 @@ public class SymbolTableFunctionEntry
         localSymbols = new SymbolTableScope();
         nestedFunctions = new HashMap< String, SymbolTableFunctionEntry >();
         localFunctions = new HashMap< String, SymbolTableFunctionEntry >();
+        parentLocals = null;
     }
     SymbolTableFunctionEntry( String n, HashMap< String, SymbolTableFunctionEntry > lf )
     {
@@ -24,6 +26,7 @@ public class SymbolTableFunctionEntry
         localSymbols = new SymbolTableScope();
         nestedFunctions = new HashMap< String, SymbolTableFunctionEntry >();
         localFunctions = lf;
+        parentLocals = null;
     }
     SymbolTableFunctionEntry( String n, SymbolTableScope st )
     {
@@ -31,6 +34,7 @@ public class SymbolTableFunctionEntry
         localSymbols = st;
         nestedFunctions = new HashMap< String, SymbolTableFunctionEntry >();
         localFunctions = new HashMap< String, SymbolTableFunctionEntry >();
+        parentLocals = null;
     }
     SymbolTableFunctionEntry( String n, SymbolTableScope st, HashMap< String, SymbolTableFunctionEntry > lf )
     {
@@ -38,6 +42,57 @@ public class SymbolTableFunctionEntry
         localSymbols = st;
         nestedFunctions = new HashMap< String, SymbolTableFunctionEntry >();
         localFunctions = lf;
+        parentLocals = null;
     }
+
+
+    /*SymbolTableFunctionEntry( String n, HashMap< String, SymbolTableFunctionEntry > pl )
+    {
+        name = n;
+        localSymbols = new SymbolTableScope();
+        nestedFunctions = new HashMap< String, SymbolTableFunctionEntry >();
+        localFunctions = new HashMap< String, SymbolTableFunctionEntry >();
+        parentLocals = pl;
+        }*/
+    SymbolTableFunctionEntry( String n, HashMap< String, SymbolTableFunctionEntry > lf, HashMap< String, SymbolTableFunctionEntry > pl )
+    {
+        name = n;
+        localSymbols = new SymbolTableScope();
+        nestedFunctions = new HashMap< String, SymbolTableFunctionEntry >();
+        localFunctions = lf;
+        parentLocals = pl;
+    }
+    /*SymbolTableFunctionEntry( String n, SymbolTableScope st, HashMap< String, SymbolTableFunctionEntry > pl )
+    {
+        name = n;
+        localSymbols = st;
+        nestedFunctions = new HashMap< String, SymbolTableFunctionEntry >();
+        localFunctions = new HashMap< String, SymbolTableFunctionEntry >();
+        parentLocals = pl;
+        }*/
+    SymbolTableFunctionEntry( String n, SymbolTableScope st, 
+                              HashMap< String, SymbolTableFunctionEntry > lf, 
+                              HashMap< String, SymbolTableFunctionEntry > pl )
+    {
+        name = n;
+        localSymbols = st;
+        nestedFunctions = new HashMap< String, SymbolTableFunctionEntry >();
+        localFunctions = lf;
+        parentLocals = pl;
+    }
+
+
+    public String getName(){ return name; }
+    public SymbolTableScope getLocalsymbols(){ return localSymbols; }
+    public HashMap< String, SymbolTableFunctionEntry > getNestedfunctions(){ return nestedFunctions; }
+    public HashMap< String, SymbolTableFunctionEntry > getLocalfunctions(){ return localFunctions; }
+    public HashMap< String, SymbolTableFunctionEntry > getParentlocals(){ return parentLocals; }
+
+    public void setName( String n ){ name = n; }
+    public void setLocalSymbols( SymbolTableScope ls ){ localSymbols = ls; }
+    public void setNestedFunctions( HashMap< String, SymbolTableFunctionEntry >  nf){ nestedFunctions = nf; }
+    public void setLocalFunctions( HashMap< String, SymbolTableFunctionEntry > lf){ localFunctions = lf; }
+    public void setParentLocals( HashMap< String, SymbolTableFunctionEntry > pl){ parentLocals = pl; }
+
 
 }
