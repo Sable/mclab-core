6a262ff7ef986f3727c9fdab56c5fe00daacbdf8
Andrew Casey <acasey@cs.mcgill.ca>
2008-07-08 23:34:33 +0000

    New category of problem.
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@561 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@539fa4c7cf06b775e488b2e773ad67b0f8bc4b67

---
+++ languages/Matlab/src/matlab/FunctionEndTranslationProblem.java
@@ -0,0 +1,7 @@
+package matlab;
+
+public class FunctionEndTranslationProblem extends TranslationProblem {
+    public FunctionEndTranslationProblem(int line, int col, String msg) {
+        super(line, col, msg);
+    }
+}
