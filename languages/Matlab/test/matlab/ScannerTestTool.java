be244da49b3b7319b282caed4e0dad1935c42cce
Andrew Casey <acasey@cs.mcgill.ca>
2008-07-18 02:39:07 +0000

    Updated to skip ExtractionParser/Grammar.
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@722 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@53444f5f842d01092c40f221a1dd36f9597d75bd

--- languages/Matlab/test/matlab/ScannerTestTool.java
+++ languages/Matlab/test/matlab/ScannerTestTool.java
@@ -1,87 +1,84 @@
 package matlab;
 
 import java.io.*;
+import java.util.List;
 
-import beaver.Scanner;
-import beaver.Symbol;
+import org.antlr.runtime.ANTLRReaderStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.Token;
 
 /**
  * A utility for producing the output file corresponding to a given input file.
  * Note that the output should be checked manually before using it as a test.
  */
 public class ScannerTestTool {
-	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.err.println("Usage: java matlab.ScannerTestTool {basename}");
-			System.exit(1);
-		}
-		String basename = args[0];
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(basename + ".in"));
-			ExtractionScanner scanner = new ExtractionScanner(in);
-			PrintWriter out = new PrintWriter(new FileWriter(basename + ".out"));
-			while(true) {
-				Symbol curr = null;
-				try {
-					curr = scanner.nextToken();
-				} catch (Scanner.Exception e) {
-					out.print('~');
-					out.print(' ');
-					out.print(e.line);
-					out.print(' ');
-					out.println(e.column);
-					break;
-				}
-				if(curr.getId() == ExtractionParser.Terminals.EOF) {
-					break;
-				}
-				printSymbol(out, curr);
-			}
-			out.close();
-			in.close();
-			System.exit(0);
-		} catch(IOException e) {
-			e.printStackTrace();
-			System.exit(2);
-		}
-	}
-	
-	public static void printSymbol(PrintWriter out, Symbol symbol) {
-		out.print(ExtractionParser.Terminals.NAMES[symbol.getId()]);
-		out.print(' ');
-		int start = symbol.getStart();
-		int startLine = Symbol.getLine(start);
-		int startCol = Symbol.getColumn(start);
-		int end = symbol.getEnd();
-		int endLine = Symbol.getLine(end);
-		int endCol = Symbol.getColumn(end);
-		if(startLine == endLine) {
-			out.print(startLine);
-			out.print(' ');
-			out.print(startCol);
-			out.print(' ');
-			out.print(endCol - startCol + 1);
-		} else {
-			out.print(startLine);
-			out.print(' ');
-			out.print(startCol);
-			out.print(' ');
-			out.print(endLine);
-			out.print(' ');
-			out.print(endCol);
-		}
-		if(symbol.value != null) {
-			out.print(' ');
-			out.print('=');
-			out.print(stringifyValue(symbol.value));
-		}
-		out.println();
-	}
-	
-	public static String stringifyValue(Object value) {
-		if(value == null) {
-			return null;
-		}
-		return value.toString().replaceAll("\n", "\\\\n").replaceAll("\r", "\\\\r");
-	}
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java matlab.ScannerTestTool {basename}");
+            System.exit(1);
+        }
+        String basename = args[0];
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(basename + ".in"));
+            MatlabLexer lexer = new MatlabLexer(new ANTLRReaderStream(in));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            PrintWriter out = new PrintWriter(new FileWriter(basename + ".out"));
+            for(Token tok : (List<Token>) tokens.getTokens()) {
+                printToken(out, tok);
+            }
+            out.close();
+            in.close();
+            System.exit(0);
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.exit(2);
+        }
+    }
+
+    public static void printToken(PrintWriter out, Token tok) throws IOException {
+        out.print(MatlabParser.tokenNames[tok.getType()]);
+        out.print(' ');
+        int startLine = tok.getLine();
+        int startCol = tok.getCharPositionInLine() + 1;
+        TextPosition lastPos = getLastPosition(tok.getText());
+        if(lastPos.getLine() == 1) {
+            out.print(startLine);
+            out.print(' ');
+            out.print(startCol);
+            out.print(' ');
+            out.print(lastPos.getColumn());
+        } else {
+            out.print(startLine);
+            out.print(' ');
+            out.print(startCol);
+            out.print(' ');
+            out.print(startLine + lastPos.getLine() - 1);
+            out.print(' ');
+            out.print(startCol + lastPos.getColumn() - 1);
+        }
+        out.print(' ');
+        out.print('=');
+        out.print(stringifyValue(tok.getText()));
+        out.println();
+    }
+
+    public static String stringifyValue(Object value) {
+        if(value == null) {
+            return null;
+        }
+        return value.toString().replaceAll("\n", "\\\\n").replaceAll("\r", "\\\\r");
+    }
+    
+    public static TextPosition getLastPosition(String text) throws IOException {
+        TrivialScanner scanner = new TrivialScanner(new StringReader(text));
+        TextPosition lastPos = null;
+        while(true) {
+            TextPosition temp = scanner.nextPos();
+            if(temp == null) {
+                break;
+            }
+            lastPos = temp;
+        }
+        return lastPos;
+    }
 }
