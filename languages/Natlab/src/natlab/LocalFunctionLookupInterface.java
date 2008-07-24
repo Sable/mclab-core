4cbd35cc0e956536d2b316baa808fcbb3059ded3
Jesse Doherty <jdoher1@cs.mcgill.ca>
2008-07-24 13:46:27 +0000

    changed getLValues to return set, added getVisible method to functions and function lists
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@799 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@9cec9e345ec24be464904c6242d623020adb4dd0

--- languages/Natlab/src/natlab/LocalFunctionLookupInterface.java
+++ languages/Natlab/src/natlab/LocalFunctionLookupInterface.java
@@ -9,4 +9,7 @@ public interface LocalFunctionLookupInterface
     public Map<String, Function> getSiblings();
     public LocalFunctionLookupInterface getParentFunction();
     public Function lookupFunction(String name);
+
+    //helper function, expensive call
+    public Map<String, Function> getVisible();
 }
