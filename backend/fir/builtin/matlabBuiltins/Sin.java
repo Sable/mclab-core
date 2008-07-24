2827f4e09a27ccc8c1268373b6d70b471d8b6fb2
Olivier Savary Belanger <osavary@cs.mcgill.ca>
2008-07-24 14:56:08 +0000

    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@801 89b902b4-d83f-0410-b942-89f7a419ffdb
    Migrated from Sable/mclab@f3ef81483f3264acf1b4ea042c95fcd7853e407d

--- backend/fir/builtin/matlabBuiltins/Sin.java
+++ backend/fir/builtin/matlabBuiltins/Sin.java
@@ -1,18 +1,10 @@
-package fir.builtin.matlabBuiltins;
+ package fir.builtin.matlabBuiltins;
 
 import fir.ast.Constant;
-import fir.type.*;
-
 
 public class Sin extends Transcendental {
 	@Override
 	public String getName() {
-		if (inputType[0].getIntrisic() instanceof Double)
-			{return "dsin";
-			}
-		if(inputTypes[0].getIntrisinc() instanceof Complex){
-			return "csin";
-		}
 		return "sin";
 			}
 
