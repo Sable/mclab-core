2c5c575c3ad25558d8c18b52d56d625e0dd5f257
Jun Li <jli127@cs.mcgill.ca>
2009-02-15 18:02:18 +0000

    update for m2f code-gen
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1062 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@1da5d49d1d994e179f95c732db9522f2e3e06cd5

--- languages/Natlab/src/natlab/SymbolTableEntry.java
+++ languages/Natlab/src/natlab/SymbolTableEntry.java
@@ -15,7 +15,14 @@ public class SymbolTableEntry
     private String original;
     private ASTNode declLocation;
     private ASTNode nodeLocation;
-    private LiteralExpr value=null;
+    private ASTNode orgNodeLocation;
+    private SymbolTableEntry orgNodeEntry;
+    private LiteralExpr value=null;		// max value
+    private LiteralExpr valueMin=null;	// min value 
+    private String valueStr="";			// min value 
+    private boolean isConstant=false;	 
+    // The old way is 'value' just keep the constant value,
+    // right now, value can store StringLiteralExpr
     
     private SymbolCount symbolCount;
 
@@ -36,13 +43,14 @@ public class SymbolTableEntry
     {
         this(s, s, n);
     }
-    public SymbolTableEntry(String s, String o, ASTNode declNode)
+    public SymbolTableEntry(String s, String o, ASTNode n)
     {
         symbol = s;
         original = o;
 
         symbolCount = new SymbolCount(); //TODO-JD: perhaps change this to search for other symbols with same original
-        declLocation=declNode;
+        nodeLocation=n;
+        // declLocation need to be set by calling setDeclLocation
     }
 
     public SymbolTableEntry split()
@@ -59,22 +67,46 @@ public class SymbolTableEntry
     public void setOriginal(String o){original=o;}
     public void setDeclLocation(ASTNode n){declLocation=n;}
     public void setNodeLocation(ASTNode n){nodeLocation=n;}
+    public void setOrgNodeLocation(ASTNode n){orgNodeLocation=n;}
+    public void setOrgNodeEntry(SymbolTableEntry e){orgNodeEntry=e;}
 
     public String getSymbol(){return symbol;}
     public String getOriginal(){return original;}
     public ASTNode getDeclLocation(){return declLocation;}
     public ASTNode getNodeLocation(){return nodeLocation;}
+    public ASTNode getOrgNodeLocation(){return orgNodeLocation;}
+    public SymbolTableEntry getOrgNodeEntry(){return orgNodeEntry;}
 
-    // Save constant value in the Symbol table	-JL 10.12
+    // Save value in the Symbol table, value can be Int/FP/String literal	-JL 12.21
     public void setValue(LiteralExpr v) {
+    	value = v;
+    	valueMin = v;
+    }
+    // For loop index variable, i=1:n, it may need the range
+    public void setValue(LiteralExpr v, LiteralExpr vmin) {	 
+    	value = v;
+    	valueMin = vmin;
+    }
+    public void setMinValue(LiteralExpr v) {	 
+    	valueMin = v;
+    }
+    public void setMaxValue(LiteralExpr v) {	 
     	value = v;
     }
     public LiteralExpr getValue() {
     	return value;
     }
-    public boolean isConstant() {
-    	return value!=null;
+    public LiteralExpr getValueMin() {	
+    	return valueMin;
     }
+    public boolean isConstant() {
+    	// return value!=null;	// The old way is 'value' just keep the constant value, 
+    	return isConstant;
+    }
+    public void setConstant(boolean flag) {
+    	isConstant = flag;
+    }
+   
    
     public boolean getXML(Document doc, Element parent)
     {   
