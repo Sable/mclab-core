172521ea9b78141e04d09da49376e85a3a336eef
Amina Aslam <amina.aslam@mail.mcgill.ca>
2009-06-24 16:31:29 +0000

    more GCD files
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1301 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@d478f6e227df68aed6e00484c861964cedddac43

---
+++ languages/Natlab/src/natlab/toolkits/analysis/ForVisitor.java
@@ -0,0 +1,126 @@
+/*.........................
+ * Loop Transformations
+ * Author:Amina Aslam.
+ * Creation Date:Jun12,2009
+ * This class implements AST visitor interface and whenever an instance of For loop is
+ * encountered or visited this class instantiates an object of annotated loop transformation
+ * class.
+ */
+package natlab.toolkits.analysis;
+
+import natlab.ast.Opt;
+import natlab.IntNumericLiteralValue;
+import natlab.ast.ASTNode;
+import natlab.ast.AssignStmt;
+import natlab.ast.ColonExpr;
+import natlab.ast.Expr;
+import natlab.ast.FPLiteralExpr;
+import natlab.ast.ForStmt;
+import natlab.ast.IfStmt;
+import natlab.ast.IntLiteralExpr;
+import natlab.ast.RangeExpr;
+import natlab.ast.Stmt;
+import natlab.ast.SwitchStmt;
+import natlab.ast.UPlusExpr;
+import DependenceAnalysis.GcdTest;
+
+//import natlab.looptransformations.LoopFusion;
+//import natlab.looptransformations.LoopInterchange;
+//import natlab.looptransformations.LoopReversal;
+//import natlab.looptransformations.LoopFission;
+//import natlab.looptransformations.LoopUnrolling;
+
+public class ForVisitor implements ASTVisitor {
+
+  	 //private static int loopNo;
+	 //private static ForStmt fStmt1;
+	 //private static ForStmt fStmt2;
+	  private String testType;
+	public ForVisitor(String tType)
+	{
+	
+		testType=tType;
+	}
+
+	/*public int getNodeID()
+	{
+		int id=fStmt2.getNodeID();
+		return id;
+	}*/
+	public void caseLoopStmt(ASTNode node){
+		
+		if (node instanceof ForStmt) {
+
+			ForStmt forNode=(ForStmt) node;
+			
+			
+			if(testType.compareTo("gcd")==0)
+			{
+					
+				GcdTest gcd=new GcdTest(forNode);
+				
+			}
+			
+			/*if(forNode.isEligibleForLoopReversal())
+	         { 
+				
+	         	LoopReversal lreversal=new LoopReversal(forNode);
+	         	lreversal.ApplyLoopReversal();
+	         }
+			 else if(forNode.isEligibleForLoopInterchange())
+	         { 
+
+	         	LoopInterchange linterchange=new LoopInterchange(forNode);
+	         	linterchange.ApplyLoopInterchange();
+	         }
+			 
+			 else if(forNode.isEligibleForLoopFission())
+	         {	
+				 
+				forNode.setStmt(new natlab.ast.Annotation(), 0);				
+	         	LoopFission lfission=new LoopFission(forNode,1);
+	         	
+	        
+	         }
+			 else if(forNode.isEligibleForLoopFusion())
+	         {
+				 
+				 if(loopNo==1)
+				  {	         	
+					 fStmt1=forNode;
+				  }
+	         	
+				 else if(loopNo==2)
+	         	  {
+					 fStmt2=forNode; 
+					 LoopFusion lfusion=new LoopFusion(fStmt1,fStmt2);
+	         		 lfusion.ApplyLoopFusion();
+	         		 
+	         		 
+	         	  }
+	          loopNo++;
+	         		
+	         }
+			 else if(forNode.isEligibleForLoopUnrolling())
+			 {
+		         	LoopUnrolling lunrolling=new LoopUnrolling(forNode);		
+		         	lunrolling.ApplyLoopUnrolling(10);
+			 }*/
+
+			
+
+		} 
+		else {
+			System.out.println("caseLoopStmt is called by "+ node.getClass().getName());
+		}
+		
+	}
+	
+	public void caseIfStmt(IfStmt node){}
+	public void caseSwitchStmt(SwitchStmt node){}
+	public void caseBranchingStmt(ASTNode node){}
+	public void caseASTNode(ASTNode node) {}
+
+
+}
+
