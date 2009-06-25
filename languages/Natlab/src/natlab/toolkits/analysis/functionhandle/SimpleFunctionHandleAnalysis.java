d80dce3c3d3fb76b94483f16f9dae0a089b236c3
Anton Dubrau <ant6nd@gmail.com>
2009-06-25 03:40:31 +0000

    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1305 89b902b4-d83f-0410-b942-89f7a419ffdb
    Migrated from Sable/mclab@280e2523827510c3fe5e61b26dd55eb0eaaa8044

---
+++ languages/Natlab/src/natlab/toolkits/analysis/functionhandle/SimpleFunctionHandleAnalysis.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package natlab.toolkits.analysis.functionhandle;
+
+import annotations.ast.ASTNode;
+import natlab.ast.AssignStmt;
+import natlab.ast.Expr;
+import natlab.toolkits.analysis.*;
+
+/**
+ * @author adubra
+ *
+ */
+abstract public class SimpleFunctionHandleAnalysis<A> {
+
+    // public SimpleFunctionHandleAnalysis(){//ASTNode tree){
+            //		super(tree);
+    //}
+	 
+	
+}
