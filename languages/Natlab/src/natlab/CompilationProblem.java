c79fcdc0e7d2f5e9b49285edef75cd43cc65bf42
Jesse Doherty <jdoher1@cs.mcgill.ca>
2009-07-02 15:55:24 +0000

    Added class for compilation errors
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1312 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@5e33a073fb4055c0908126134b5b8f3d8ca87dc2

---
+++ languages/Natlab/src/natlab/CompilationProblem.java
@@ -0,0 +1,58 @@
+package natlab;
+
+/**
+ * Describes the positon and nature of a problem encountered while compiling
+ * source. This can include matlab to natlab translation problems, parsing 
+ * problems, and general compilation problems
+ */
+
+public class CompilationProblem 
+{
+    private final boolean located;
+    private final int line;
+    private final int col;
+    private final String msg;
+
+    public CompilationProblem( int line, int col, String msg )
+    {
+        this.line = line;
+        this.col = col;
+        this.msg = msg;
+        this.located = true;
+    }
+    public CompilationProblem( String msg )
+    {
+        this.line = -1;
+        this.col = -1;
+        this.msg = msg;
+        this.located = false;
+    }
+
+    public boolean hasLocation()
+    {
+        return located;
+    }
+
+    public int getLine()
+    {
+        return line;
+    }
+
+    public int getColumn()
+    {
+        return col;
+    }
+
+    public String getMessage()
+    {
+        return msg;
+    }
+
+    public String toString() 
+    {
+        if( located )
+            return "[" + line + ", " + col + "]  " + msg;
+        else
+            return msg;
+    }
+}
