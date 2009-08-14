8bc3f698ddbfa44be13fb1fff8bf505b09126240
Anton Dubrau <ant6nd@gmail.com>
2009-08-14 22:18:58 +0000

    added simple test analysis
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1403 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@fefff5d522c11fc9cb6cee5f37ad04b64730cb48

--- languages/Natlab/src/natlab/toolkits/analysis/functionhandle/SimpleFunctionHandleAnalysis.java
+++ languages/Natlab/src/natlab/toolkits/analysis/functionhandle/SimpleFunctionHandleAnalysis.java
@@ -4,15 +4,13 @@
 package natlab.toolkits.analysis.functionhandle;
 
 import annotations.ast.ASTNode;
-import natlab.ast.AssignStmt;
-import natlab.ast.Expr;
 import natlab.toolkits.analysis.*;
 
 /**
  * @author adubra
  *
  */
-abstract public class SimpleFunctionHandleAnalysis<A> {
+abstract public class SimpleFunctionHandleAnalysis {//extends AbstractSimpleStructuralForwardAnalysis<A>  {
 
     // public SimpleFunctionHandleAnalysis(){//ASTNode tree){
             //		super(tree);
---
+++ languages/Natlab/src/natlab/toolkits/analysis/functionhandle/TestAnalysis.java
@@ -0,0 +1,74 @@
+package natlab.toolkits.analysis.functionhandle;
+
+import natlab.FlowAnalysisTestTool;
+import natlab.ast.ASTNode;
+import natlab.ast.AssignStmt;
+import natlab.ast.Expr;
+import natlab.toolkits.analysis.*;
+
+
+public class TestAnalysis extends AbstractSimpleStructuralForwardAnalysis<HashSetFlowSet<String>> {
+
+	public static void main(String[] args) throws Exception {
+		String source = "/home/2005/adubra/McLab/matlabtests/test.m";
+		FlowAnalysisTestTool tool = new FlowAnalysisTestTool(source,TestAnalysis.class);
+		System.out.println(tool.run());
+	}
+	
+	
+	
+    public TestAnalysis(ASTNode tree){
+        super( tree );
+    }
+	
+    
+    @Override
+    public void caseAssignStmt(AssignStmt node) {
+		copy(currentInSet, currentOutSet);
+		for (String s : node.getLValues()){
+			currentOutSet.add(s);
+		}
+    }
+    
+    
+	@Override
+	public void caseCondition(Expr condExpr) {
+		copy(currentInSet, currentOutSet);
+	}
+
+	@Override
+	public void caseLoopVarAsCondition(AssignStmt loopVar) {
+		copy(currentInSet, currentOutSet);
+	}
+
+	@Override
+	public void caseLoopVarAsInit(AssignStmt loopVar) {
+		copy(currentInSet, currentOutSet);
+	}
+
+	@Override
+	public void caseLoopVarAsUpdate(AssignStmt loopVar) {
+		copy(currentInSet, currentOutSet);
+	}
+
+	@Override
+	public void copy(HashSetFlowSet<String> source, HashSetFlowSet<String> dest) {
+		currentInSet.copy(currentOutSet);
+	}
+
+	@Override
+	public void merge(HashSetFlowSet<String> in1, HashSetFlowSet<String> in2, HashSetFlowSet<String> out) {
+		HashSetFlowSet<String> out2 = new HashSetFlowSet<String>();
+		copy(in1,out2);
+		out2.union(in2);
+		copy(out2,out);
+	}
+
+	@Override
+	public HashSetFlowSet<String> newInitialFlow() {
+		return new HashSetFlowSet<String>();
+	}
+
+}
+
+
