94a89928e93f309d452cfa90d8109a3620de4436
Jesse Doherty <jdoher1@cs.mcgill.ca>
2010-04-26 17:47:12 +0000

    Completed version of MultiReturnRewrite
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1641 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@19057360419c7f4fec071cfcbcf4dd3ef71bcd63

--- languages/Natlab/src/natlab/toolkits/rewrite/AbstractLocalRewrite.java
+++ languages/Natlab/src/natlab/toolkits/rewrite/AbstractLocalRewrite.java
@@ -3,33 +3,95 @@ package natlab.toolkits.rewrite;
 import ast.*;
 import natlab.toolkits.analysis.AbstractNodeCaseHandler;
 
+import java.lang.UnsupportedOperationException;
+
 public abstract class AbstractLocalRewrite extends AbstractNodeCaseHandler
 {
 
     protected ASTNode oldTree, newTree;
 
-    protected ASTNode newNode = null;
+    protected TransformedNode newNode = null;
 
     protected java.util.List multiNodes = null;
+
+    /**
+     * Applies the rewrite to a particular node. Also does some book
+     * keeping.
+     */
+    protected void rewrite( ASTNode node )
+    {
+        newNode = null;
+        node.analyze(this);
+    }
+
+    /**
+     * Applies the rewrite to all children of the given node. 
+     */
+    protected void rewriteChildren( ASTNode node )
+    {
+        caseASTNode( node );
+    }
 
     public AbstractLocalRewrite( ASTNode tree )
     {
         oldTree = tree;
     }
 
-    //protected 
+
+    public ASTNode transform()
+    {
+        rewrite( oldTree );
+        ASTNode returnNode;
+        if( newNode == null ){
+            returnNode = oldTree;
+        }
+        else{
+            returnNode = newNode.getSingleNode();
+        }
+        return returnNode;
+        
+    }
 
     public void caseASTNode( ASTNode node )
     {
         int numChild = node.getNumChild();
         for( int i = 0; i<numChild; i++ ){
-            newNode = null;
-            node.getChild(i).analyze(this);
+            rewrite( node.getChild(i) );
             if( newNode != null )
-                node.setChild( newNode, i );
+                if( newNode.isSingleNode() )
+                    node.setChild( newNode.getSingleNode(), i );
+                else{
+                    String msg = "Generic transformation case received non single nodes from " +
+                        "transforming a child node. This should only happen when the current " +
+                        "case is for a list or otherwise expects this behavior.";
+                    throw new UnsupportedOperationException(msg);
+                }
         }
         newNode = null;
     }
 
-    //public void caseList( List node )
+    public void caseList( List node )
+    {
+        for( int i=0; i<node.getNumChild(); i++ ){
+            ASTNode child = node.getChild(i);
+            rewrite(child);
+            if( newNode != null )
+                if( newNode.isSingleNode() )
+                    node.setChild( newNode.getSingleNode(), i );
+                else if( newNode.isEmptyNode() ){
+                    node.removeChild( i );
+                    i--;
+                }
+                else{
+                    java.util.List<ASTNode> nodes = newNode.getMultipleNodes();
+                    node.removeChild(i);
+                    for( ASTNode newChild : nodes ){
+                        node.insertChild( newChild, i );
+                        i++;
+                    }
+                    i--;
+                }
+        }
+        newNode = null;
+    }
 }
--- languages/Natlab/src/natlab/toolkits/rewrite/multireturn/MultiReturnRewrite.java
+++ languages/Natlab/src/natlab/toolkits/rewrite/multireturn/MultiReturnRewrite.java
@@ -2,7 +2,10 @@ package natlab.toolkits.rewrite.multireturn;
 
 import ast.*;
 import natlab.toolkits.analysis.AbstractNodeCaseHandler;
-import natlab.toolkits.rewrite.AbstractLocalRewrite;
+import natlab.toolkits.rewrite.*;
+import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.ArrayList;
 
 public class MultiReturnRewrite extends AbstractLocalRewrite
 {
@@ -12,26 +15,63 @@ public class MultiReturnRewrite extends AbstractLocalRewrite
         super( tree );
     }
 
-    public ASTNode go()
-    {
-        newNode = null;
-        oldTree.analyze( this );
 
-        ASTNode returnNode;
-        if( newNode == null ){
-            returnNode = oldTree;
-        }
-        else{
-            returnNode = newNode;
-        }
-        return returnNode;
+
+    public void caseAssignStmt( AssignStmt node )
+    {
+        rewriteChildren( node );
         
-    }
+        
+        // Stmt[] stmts = new Stmt[2];
+        // Expr lhs = node.getLHS();
+        // Expr rhs = node.getRHS();
 
-    public void caseName( Name node )
-    {
-        Name newName = new Name( node.getID() + "JESSE" );
-        newNode = newName;
-    }
+        // System.out.println( lhs.getClass() );
+        
+        // Name[] tmps = TempFactory.genFreshTempName(2);
+            
+        // stmts[0] = new AssignStmt( new NameExpr( tmps[0] ), rhs );
+        // stmts[1] = new AssignStmt( lhs, new NameExpr( tmps[1] ) );
 
+        // stmts[0].setOutputSuppressed( true );
+        // stmts[1].setOutputSuppressed( node.isOutputSuppressed() );
+        // newNode = new TransformedNode( stmts );
+        
+
+        if( node.getLHS() instanceof MatrixExpr ){
+            //NOTE: assume lhs is a matrix expr with exactly 1 row
+            Row lhs = ((MatrixExpr)node.getLHS()).getRow(0);
+            Expr rhs = node.getRHS();
+
+            HashSet<String> names = new HashSet<String>(lhs.getNumElement());
+            LinkedList<AssignStmt> newStmts = new LinkedList<AssignStmt>();
+            ArrayList<Expr> lvalues = new ArrayList<Expr>( lhs.getNumElement() );
+
+            for( Expr e : lhs.getElements() ){
+                if( e instanceof NameExpr ){
+                    String name = ((NameExpr)e).getName().getID();
+                    if( !names.contains( name ) ){
+                        names.add(name);
+                        lvalues.add( e );
+                        continue;
+                    }
+                }
+                TempFactory tmp = TempFactory.genFreshTempFactory();
+                lvalues.add( tmp.genNameExpr() );
+                AssignStmt newAssign = new AssignStmt( e, tmp.genNameExpr() );
+                newAssign.setOutputSuppressed( true );
+                newStmts.add( newAssign );
+            }
+            if( newStmts.size() > 0 ){
+                Row newLHS = new Row( new List() );
+                for( Expr e : lvalues )
+                    newLHS.getElements().add( e );
+                AssignStmt newAssign = new AssignStmt( new MatrixExpr( (new List()).add(newLHS) ),
+                                                       rhs );
+                newAssign.setOutputSuppressed( node.isOutputSuppressed() );
+                newStmts.add( 0, newAssign );
+                newNode = new TransformedNode( newStmts );
+            }
+        }
+    }
 }
