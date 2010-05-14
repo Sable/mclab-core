9e4cb92404fbf8dabaf6d65b4bf962d70b2f685f
Jesse Doherty <jdoher1@cs.mcgill.ca>
2010-05-14 16:36:21 +0000

    Added some documentation comments
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1662 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@e87d7dde1ab1c51c9091ad46287eb553ae644d4f

--- languages/Natlab/src/natlab/toolkits/rewrite/multireturn/MultiReturnRewrite.java
+++ languages/Natlab/src/natlab/toolkits/rewrite/multireturn/MultiReturnRewrite.java
@@ -7,6 +7,16 @@ import java.util.LinkedList;
 import java.util.HashSet;
 import java.util.ArrayList;
 
+/**
+ * A rewrite to simplify multi return assignment statements. The
+ * rewrite results in all multi return assignments having only simple
+ * name expressions appearing at most once on the left hand side. It
+ * replaces all complex lvalues with temporaries and then assigns the
+ * temporaries to the appropriate lvalue afterwards. 
+ *
+ * Note: this does not simplify the complex lvalue expressions at
+ * all. This is left to other rewrites.
+ */
 public class MultiReturnRewrite extends AbstractLocalRewrite
 {
 
@@ -28,6 +38,7 @@ public class MultiReturnRewrite extends AbstractLocalRewrite
             ArrayList<Expr> lvalues = new ArrayList<Expr>( lhs.getNumElement() );
 
             buildTempStatements( newStmts, lvalues, lhs.getElements() );
+
             if( newStmts.size() > 0 ){
                 AssignStmt newAssign = buildNewAssignment( lvalues, rhs, node.isOutputSuppressed() );
                 newStmts.add( 0, newAssign );
@@ -76,6 +87,8 @@ public class MultiReturnRewrite extends AbstractLocalRewrite
      * @param rhs              the rhs expression of original assignment
      * @param outputSuppressed boolean used to suppress output of new
      *                         assignment 
+     *
+     * @return the new AssignStmt generated
      */
     private AssignStmt buildNewAssignment( ArrayList<Expr> lvalues, Expr rhs, boolean outputSuppressed )
     {
