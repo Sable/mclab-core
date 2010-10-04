1797cb0ab7057412a183d0fefbacf2789805aede
Jesse Doherty <jdoher1@cs.mcgill.ca>
2010-10-04 00:49:52 +0000

    Added the CallGraph class and modified CallGraphBuilder to use it. Also added some comments to the builder to clarify the various maps contained within.
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1722 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@8df23e47dae9be5da59fc58e3e3cd4c83e2f5484

---
+++ languages/Natlab/src/natlab/toolkits/analysis/callgraph/CallGraph.java
@@ -0,0 +1,78 @@
+package natlab.toolkits.analysis.callgraph;
+
+import java.util.*;
+import ast.*;
+
+public class CallGraph
+{
+
+    protected HashMap<ASTNode, TreeSet<CallSiteLabel>> programLabelMap;
+    protected HashMap<ASTNode, CallSiteLabel> labelMap;
+    protected HashMap<CallSiteLabel, MayMustTreeSet<ASTNode>> targetMap;
+
+    public CallGgraph( HashMap<ASTNode, TreeSet<CallSiteLabel>> programLabelMap,
+                       HashMap<ASTNode, CallSiteLabel> labelMap,
+                       HashMap<CallSiteLabel, MayMustTreeSet<ASTNode>> targetMap )
+    {
+        this.programLabelMap = programLabelMap;
+        this.labelMap = labelMap;
+        this.targetMap = targetMap;
+    }
+
+    /**
+     * Gets all the call site labels for a given callable. A callable
+     * is either a function or a script.
+     */
+    public TreeSet<CallSiteLabel> getCallSiteLabelsForCallables( ASTNode n )
+    {
+        return programLabelMap.get(ASTNode n);
+    }
+    /**
+     * Gets call site label for a given call node, either a
+     * parametrized expression or a name expression.
+     */
+    public CallSiteLabel getCallSiteLabel( ASTNode n )
+    {
+        return labelMap.get( n );
+    }
+
+    /**
+     * Gets call targets by the call site label.
+     */
+    public MayMustTreeSet<ASTNode> getTargets( CallSiteLabel )
+    {
+        return targetMap.get( label );
+    }
+
+    /**
+     * Gets call targets for a given call node. Such nodes can be
+     * parameterized expressions or name expressions. This is
+     * equivalent to calling getTargets( getCallSiteLabel( site ))
+     * when the given site is a call site. 
+     */
+    public MayMustTreeSet<ASTNode> getTargets(ASTNode site)
+    {
+        CallSiteLabel label = labelMap.get(site);
+        if( label == null )
+            return null;
+        else
+            return targetMap.get( label );
+    }
+    /**
+     * Checks to see if the given node might represent a call site.
+     */
+    public boolean isPossibleCallSite(ASTNode site)
+    {
+        MayMustTreeSet targets = getTargets( site );
+        return targets != null;
+    }
+
+    /**
+     * Checks to see if the given node is definitely a call site.
+     */
+    public boolean isDefiniteCallSite(ASTNode site)
+    {
+        MayMustTreeSet targets = getTargets( site );
+        return targets != null && targets.isMust();
+    }
+}
