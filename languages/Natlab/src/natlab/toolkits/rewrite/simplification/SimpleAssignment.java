eabfedbc2db2d4ead48e29499f0f21486c722090
Jesse Doherty <jdoher1@cs.mcgill.ca>
2011-02-16 17:26:02 +0000

    Added a new rewrite to perform simplification on assignments.
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1790 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@11a0c760fd6b64dce954b1056993f4b275e99f84

---
+++ languages/Natlab/src/natlab/toolkits/rewrite/simplification/SimpleAssignment.java
@@ -0,0 +1,63 @@
+package natlab.toolkits.rewrite.simplification;
+
+import java.util.*;
+
+import ast.*;
+import natlab.DecIntNumericLiteralValue;
+import natlab.toolkits.rewrite.*;
+import natlab.toolkits.analysis.varorfun.*;
+
+
+/**
+ * @author Jesse Doherty
+ */
+public class SimpleAssignment extends AbstractSimplification
+{
+    public SimpleAssignment( ASTNode tree, VFPreorderAnalysis kind )
+    {
+        super( tree, kind );
+    }
+
+    /**
+     * Builds a singleton start set containing this class.
+     */ 
+    public static Set<Class<? extends AbstractSimplification>> getStartSet()
+    {
+        HashSet<Class<? extends AbstractSimplification>> set = new HashSet();
+        set.add( SimpleAssignment.class );
+        return set;
+    }
+
+    public Set<Class<? extends AbstractSimplification>> getDependencies()
+    {
+        HashSet<Class<? extends AbstractSimplification>> dependencies = new HashSet();
+        return dependencies;
+    }
+
+    /*
+      Exp1 = Exp2
+      =========
+      t1 = Exp2;
+      Exp1 = t1
+      ---------
+      if Exp1 and Exp2 are both not simple variables
+
+      Does not deal with multi assignment statements.
+     */
+    public void caseAssignStmt( AssignStmt node )
+    {
+        Expr lhs = node.getLHS();
+        Expr rhs = node.getRHS();
+        if( !(lhs instanceof MatrixExpr) ){
+            if( !isVar(lhs) && !isVar(rhs) ){
+                TempFactory tempF = TempFactory.genFreshTempFactory();
+                AssignStmt a1 = new AssignStmt( tempF.genNameExpr(), rhs );
+                AssignStmt a2 = new AssignStmt( lhs, tempF.genNameExpr() );
+                
+                newNode = new TransformedNode( a1 );
+                newNode.add(a2);
+            }
+        }
+    }
+
+}
