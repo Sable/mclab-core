09d27048e079be040e2085c7ea738f7982427488
Jesse Doherty <jdoher1@cs.mcgill.ca>
2011-03-08 20:52:28 +0000

    Added short circuit simplification functionality to right simplification.
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1800 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@f88c83ea92499dcee518c95791d681fc90dba331

--- languages/Natlab/src/natlab/toolkits/rewrite/simplification/RightSimplification.java
+++ languages/Natlab/src/natlab/toolkits/rewrite/simplification/RightSimplification.java
@@ -76,18 +76,31 @@ public class RightSimplification extends AbstractSimplification
 
     protected LinkedList<Stmt> newStmts;
 
+    /**
+     * Takes note that it is in an ExprStmt, then forwards to
+     * caseStmt. 
+     */
     public void caseExprStmt( ExprStmt node )
     {
-        inExprStmt = true;
         caseStmt( node );
+
         inExprStmt = false;
     }
+
+    /**
+     * Takes note that it is in an AssignStmt, then forwards to
+     * caseStmt or simplifySC. 
+     */
     public void caseAssignStmt( AssignStmt node )
     {
         inAssignStmt = true;
+
         caseStmt( node );
+
         inAssignStmt = false;
     }
+    protected boolean inSCStmt = false;
+
     public void caseStmt( Stmt node )
     {
         newStmts = new LinkedList();
@@ -109,7 +122,7 @@ public class RightSimplification extends AbstractSimplification
         LinkedList<Stmt> backNewStmts = newStmts;
         LinkedList<Stmt> exprsNewStmts = new LinkedList();
 
-        //loop through all the new assignment stmts and rewrite them
+        //loop through all the new assignment stmts and rewrites them
         while( !ec.getNewAssignments().isEmpty() ){
             
             Stmt workStmt = ec.getNewAssignments().removeLast();
@@ -130,8 +143,166 @@ public class RightSimplification extends AbstractSimplification
         newStmts = backNewStmts;
         if( !exprsNewStmts.isEmpty() ){
             newStmts.addAll( exprsNewStmts );
-            newNode = new TransformedNode( newExpr );
+            if( !inSCStmt )
+                newNode = new TransformedNode( newExpr );
         }
+        if( currentSCTempFact!=null ){
+            AssignStmt newAssign = new AssignStmt( currentSCTempFact.genNameExpr(),
+                                                   newExpr );
+            newAssign.setOutputSuppressed(true);
+            newStmts.add( newAssign );
+            CheckScalarStmt newCheck = new CheckScalarStmt( currentSCTempFact.genNameExpr() );
+            newCheck.setOutputSuppressed(true);
+            newStmts.add( newCheck );
+            newNode = new TransformedNode( currentSCTempFact.genNameExpr() );
+        }
+
         //deep = deep.substring(0,deep.length()-1);
+    }
+
+    protected TempFactory currentSCTempFact;
+
+
+    protected boolean setupSCSimp()
+    {
+        if( currentSCTempFact == null ){
+            currentSCTempFact = TempFactory.genFreshTempFactory();
+            return true;
+        }
+        else
+            return false;
+    }
+    protected void teardownSCSimp(boolean isBase)
+    {
+        if( isBase )
+            currentSCTempFact = null;
+    }
+    /**
+     * Simplifies assignments with short-circuit expressions on the
+     * rhs. 
+     *
+     * Should only be given an AssignStmt with a short-circuit
+     * expression on the rhs.
+     */
+    protected void simplifySC( AssignStmt node )
+    {
+        Expr lhs = node.getLHS();
+        Expr rhs = node.getRHS();
+
+
+        currentSCTempFact = TempFactory.genFreshTempFactory();
+    }
+
+    protected void simplifySC( ExprStmt node )
+    {
+        Expr expr = node.getExpr();
+
+        currentSCTempFact = TempFactory.genFreshTempFactory();
+    }
+
+    public void caseShortCircuitAndExpr( ShortCircuitAndExpr node )
+    {
+        Expr e1 = node.getLHS();
+        Expr e2 = node.getRHS();
+
+        simplifySCIfPat( e1, e2, false );
+    }
+
+    public void caseShortCircuitOrExpr( ShortCircuitOrExpr node )
+    {
+        Expr e1 = node.getLHS();
+        Expr e2 = node.getRHS();
+
+        simplifySCIfPat( new NotExpr(e1), e2, true );
+    }
+
+    public void caseNotExpr( NotExpr node )
+    {
+        Expr e = node.getOperand();
+
+        if( e instanceof NotExpr )
+            rewrite( ((NotExpr)e).getOperand() );
+        else if( e instanceof ShortCircuitAndExpr ){
+            Expr e1 = ((ShortCircuitAndExpr)e).getLHS();
+            Expr e2 = ((ShortCircuitAndExpr)e).getRHS();
+
+            simplifySCIfPat( e1, new NotExpr(e2), true );
+        }
+        else if( e instanceof ShortCircuitOrExpr ){
+            Expr e1 = ((ShortCircuitOrExpr)e).getLHS();
+            Expr e2 = ((ShortCircuitOrExpr)e).getRHS();
+
+            caseShortCircuitAndExpr( new ShortCircuitAndExpr( new NotExpr( e1 ),
+                                                              new NotExpr( e2 )));
+        }
+        else
+            caseExpr(node);
+    }
+
+    protected void simplifySCIfPat( Expr e1, Expr e2, boolean elseVal )
+    {
+        boolean isBaseSC = setupSCSimp();
+        LinkedList<Stmt> thenPartList = makeThenPart( e2 );
+        simplifySCIfPat( e1, thenPartList, elseVal );
+        teardownSCSimp(isBaseSC);
+    }
+    /**
+     * Creates the then part contents for simplifying SC
+     * expressions. This is used to transition from the expression
+     * simplification to the if simplification for SC simplification.
+     */
+    protected LinkedList<Stmt> makeThenPart( Expr e )
+    {
+        LinkedList<Stmt> backupNewStmts = newStmts;
+        newStmts = new LinkedList();
+
+        rewrite(e);
+
+        LinkedList<Stmt> thenPartList = newStmts;
+        newStmts = backupNewStmts;
+        return thenPartList;
+    }
+    protected void simplifySCIfPat( Expr e, LinkedList<Stmt> thenPart, boolean elseVal )
+    {
+        simplifySCIfPat( e, 
+                         ASTHelpers.listToList(thenPart), elseVal );
+        //new ast.List<Stmt>().add( boolAssign ) );
+    }
+
+    protected void simplifySCIfPat( Expr e, ast.List<Stmt> thenPart, boolean elseVal )
+    {
+        if( e instanceof ShortCircuitAndExpr ){
+            Expr e1 = ((ShortCircuitAndExpr)e).getLHS();
+            Expr e2 = ((ShortCircuitAndExpr)e).getRHS();
+
+            LinkedList<Stmt> backupNewStmts = newStmts;
+            newStmts = new LinkedList();
+
+            
+            simplifySCIfPat( e2, thenPart, elseVal );
+
+            newStmts = backupNewStmts;
+
+            simplifySCIfPat( e1, ASTHelpers.listToList(newStmts), elseVal );
+        }
+        else if( e instanceof NotExpr && ( ((NotExpr)e).getOperand() instanceof ShortCircuitOrExpr )){
+            ShortCircuitOrExpr orExpr = (ShortCircuitOrExpr)((NotExpr)e).getOperand();
+            Expr e1 = orExpr.getLHS();
+            Expr e2 = orExpr.getRHS();
+            ShortCircuitAndExpr andExpr = new ShortCircuitAndExpr( new NotExpr(e1), new NotExpr(e2) );
+
+            simplifySCIfPat( andExpr, thenPart, elseVal );
+            return;
+        }
+        else{
+            rewrite(e);
+            e = currentSCTempFact.genNameExpr();
+            AssignStmt boolAssign = new AssignStmt(currentSCTempFact.genNameExpr(), 
+                                                   ASTHelpers.buildBoolLit(elseVal) );
+            boolAssign.setOutputSuppressed(true);
+
+            newStmts.add( ASTHelpers.newIfStmt( e, thenPart, new ast.List<Stmt>().add(boolAssign) ) );
+        }
+
     }
 }
