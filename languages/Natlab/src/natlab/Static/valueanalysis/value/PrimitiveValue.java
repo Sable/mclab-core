ec2cc5faae3fd58365645dbd935e5c39f36a1961
Anton Dubrau <ant6nd@gmail.com>
2011-06-10 22:50:07 +0000

    value analysis update
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1890 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@f7eb87adf8b551d2979155180808c4468806067d

--- languages/Natlab/src/natlab/Static/valueanalysis/value/PrimitiveValue.java
+++ languages/Natlab/src/natlab/Static/valueanalysis/value/PrimitiveValue.java
@@ -1,7 +1,7 @@
 package natlab.Static.valueanalysis.value;
 
 import natlab.Static.classes.reference.PrimitiveClassReference;
-import natlab.Static.valueanalysis.constant.Constant;
+import natlab.Static.valueanalysis.constant.*;
 
 /**
  * represents a primitive value, i.e. a value that has a primitive type.
@@ -14,23 +14,15 @@ public class PrimitiveValue implements AbstractValue {
     PrimitiveClassReference classRef;
     Constant constant;
     
-    protected PrimitiveValue(PrimitiveClassReference classRef,Constant constant){
-        this.classRef = classRef;
-        this.constant = constant;
-    }
-    
     @Override
     public PrimitiveClassReference getMatlabClass() {
         return classRef;
     }
 
-    public Constant getConstant(){
-        return constant;
-    }
     
     @Override
     public AbstractValue union(AbstractValue other) {
         if (!other.getClass().equals(classRef)) return null;
-        return new PrimitiveValue(classRef,constant.union(((PrimitiveValue)other).constant));
+        return null; //TODO
     }
 }
