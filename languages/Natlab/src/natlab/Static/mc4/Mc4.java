d2716abe1f282087d8500c04173c1201154fb243
Anton Dubrau <ant6nd@gmail.com>
2011-12-14 02:59:17 +0000

    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2061 89b902b4-d83f-0410-b942-89f7a419ffdb
    Migrated from Sable/mclab@47e213f743e1424a5ae8d6e701bfdd367ffe5db5

--- languages/Natlab/src/natlab/Static/mc4/Mc4.java
+++ languages/Natlab/src/natlab/Static/mc4/Mc4.java
@@ -23,19 +23,18 @@ package natlab.Static.mc4;
 
 import java.io.*;
 import java.net.URL;
+import java.util.HashSet;
 
 
-import natlab.*;
+import natlab.FlowAnalysisTestTool;
 import natlab.Static.builtin.*;
 import natlab.Static.callgraph.*;
-import natlab.Static.interproceduralAnalysis.InterproceduralAnalysis;
-import natlab.Static.interproceduralAnalysis.examples.CallStringAnalysis;
-import natlab.Static.valueanalysis.IntraproceduralValueAnalysis;
-import natlab.Static.valueanalysis.ValueSet;
+import natlab.Static.classes.reference.*;
+import natlab.Static.valueanalysis.*;
 import natlab.Static.valueanalysis.simplematrix.SimpleMatrixValue;
-import natlab.Static.valueanalysis.value.MatrixValueFactory;
+import natlab.Static.valueanalysis.simplematrix.SimpleMatrixValueFactory;
+import natlab.Static.valueanalysis.value.*;
 import natlab.options.Options;
-import natlab.toolkits.analysis.functionhandle.FunctionReference;
 import natlab.toolkits.filehandling.genericFile.*;
 import natlab.toolkits.path.*;
 
@@ -58,7 +57,6 @@ public class Mc4 {
         if (PRINT_STACK_ON_ERROR) Thread.dumpStack();
         if (EXIT_ON_ERROR) System.exit(1);
     }
-    public static FilePathEnvironment functionFinder;
     
     
     //TODO get rid of this
@@ -78,7 +76,6 @@ public class Mc4 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	    
         //for now just open a file if no option is given .. eclipse is a pain
 		Options options = new Options();
 		options.parse(args);
@@ -97,12 +94,44 @@ public class Mc4 {
 		
 		
 		//try to do all benchmarks
-		String bFolderString = "C:\\classes\\mclab\\Benchmarks\\matlabBenchmarks";
-		File bFolder = new File(bFolderString);
-		for (File dir : bFolder.listFiles()){
+		String bFolderString = "C:\\classes\\mclab\\Benchmarks\\matlabBenchmarks\\";
+		String[] subfolders = new String[]{
+		    "aaatest",
+		    "adpt",
+		    "beul",
+		    "capr",
+		    "clos",
+		    "crni",
+		    "dich",
+		    "diff",
+		    "edit", //uses cell arrays
+		    //"eigenval", //the benchmark is broken!
+		    "fdtd",
+		    "fft", 
+		    "fiff",
+		    //"fourier", //the benchmark is broken!
+		    "hnormalise",
+		    "lagrcheb",
+		    "linear",
+		    "mbrt",
+		    "mils",
+		    "nb1d",
+		    "nb3d",
+		    "nfrc", // uses &&
+		    "nnet", //uses cell arrays
+		    "play", //recursive
+		    "rayt", //end-issue???
+		    "sch2", //error in toeplitz - nargin
+		    "schr", //uses lambda - toeplitz
+		    //"sdku", //uses load
+		    "spga", //end-issue???
+		    "svd" //uses nargout - cannot be all inlined
+		    };
+		for (String subfolder : subfolders){
+		    File dir = new File(bFolderString+subfolder);
 		    File[] list = dir.listFiles(new FilenameFilter() {
                 public boolean accept(File dir, String name) {
-                    return name.matches("drv_.*m");
+                    return name.matches(".*drv_.*m");
                 }
             });
 		    if (list == null || list.length == 0) continue;
@@ -114,6 +143,8 @@ public class Mc4 {
 	        main(options);
 	        System.exit(0);//only do first
 		}
+		System.out.println("\n---done---\n builtins: "+allRefs.size()+"\n\n");
+		//for (FunctionReference ref : allRefs) System.out.println(ref);
 		System.exit(0);
 		/* */
 		
@@ -133,6 +164,8 @@ public class Mc4 {
 		main(options);
 	}
 
+	static HashSet<FunctionReference> allRefs = new HashSet<FunctionReference>();
+	
 	/**
 	 * compiles matlab files into fortran, using the Options as argument.
 	 * This is basically the entry point for mc4.
@@ -141,57 +174,32 @@ public class Mc4 {
 	public static void main(Options options){
         //object that resolves function names to files      
 	    long t = System.currentTimeMillis();
+	    FilePathEnvironment functionFinder;
         functionFinder = new FilePathEnvironment(options, Builtin.getBuiltinQuery());
         //System.out.println("time "+(System.currentTimeMillis() - t));
         //System.exit(0);
         
-	    //collect all need matlab files
+	    //collect all needed matlab files
 	    FunctionCollection functions = new FunctionCollection(functionFinder);
+	    allRefs.addAll(functions.getAllFunctionBuiltinReferences());
 	    
-	    /** test code for interproc
-	    InterproceduralAnalysis<?,?,?> analysis = 
-	        InterproceduralAnalysis.create(new CallStringAnalysis.Factory(), functions, null);
-	    System.out.println(analysis.getMainNode().getResult());
-	    System.exit(0);
-	    //inline all
-	    //functions.inlineAll();
-	    */
 	    
-	    //print result for now
-	    //System.out.println(functions.getAsInlinedFunction().getPrettyPrinted());
-	   
-	    /**
-	    // just write out file
-	    if (true){
-	        FileWriter fstream = null;
-	        try{
-	            ast.Function function = functions.getAsInlinedFunction();
-	            System.out.println("get inilined function success");
-	            if (!(new File(outDir).exists())){ (new File(outDir)).mkdir(); }
-	            File file = new File(outDir+"/"+function.getName()+".m");
-	            System.out.println("writing file "+file.getAbsolutePath());
-	            fstream = new FileWriter(file);
-	            fstream.write(function.getPrettyPrinted());
-                fstream.close();	            
-	        } catch(IOException e){
-	            System.err.println("output failed: "+e.getMessage());
-	        } catch (UnsupportedOperationException e){
-	            System.err.println(e.getMessage());
-	            e.printStackTrace();
-	        }
-	    }
-        */
-	    
-	    StaticFunction aFunction = functions.getAsInlinedStaticFunction();
-	    
-	    System.out.println(aFunction);
 	    
 	    //test intraprocedural class analysis
-	    IntraproceduralValueAnalysis<SimpleMatrixValue> classAnalysis =
-	        new IntraproceduralValueAnalysis<SimpleMatrixValue>(
-	            aFunction,SimpleMatrixValue.FACTORY);
-	    FlowAnalysisTestTool test = new FlowAnalysisTestTool(classAnalysis);
-	    System.out.println(test.run(true,true));
+	    try{
+	    ValueAnalysis<SimpleMatrixValue> analysis = new ValueAnalysis<SimpleMatrixValue>(
+	            functions,
+	            Args.newInstance(new SimpleMatrixValue(PrimitiveClassReference.DOUBLE)),
+	            new SimpleMatrixValueFactory());
+        System.out.println(analysis);
+        System.out.println(analysis.getPrettyPrinted());
+	    } catch (StackOverflowError e){
+	        System.err.println("stackoverflow in "+functions.getMain().name);
+	        System.exit(0);
+	    }
+	    System.out.println("=> finished "+functions.getMain().name);
+	    //FlowAnalysisTestTool test = new FlowAnalysisTestTool(classAnalysis);
+	    //System.out.println(test.run(true,true));
 	}
 }
 
