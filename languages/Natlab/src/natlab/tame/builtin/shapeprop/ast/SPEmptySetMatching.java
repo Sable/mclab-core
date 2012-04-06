9817f23db95941d34d7a25f6e064438368a729a8
Xu Li <aaronleemcgill@gmail.com>
2012-04-06 19:40:09 +0000

    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2101 89b902b4-d83f-0410-b942-89f7a419ffdb
    Migrated from Sable/mclab@c0652b10137f5d4beb85a8bc258bbb9bd45cf6ad

---
+++ languages/Natlab/src/natlab/tame/builtin/shapeprop/ast/SPEmptySetMatching.java
@@ -0,0 +1,14 @@
+package natlab.tame.builtin.shapeprop.ast;
+
+public class SPEmptySetMatching extends SPAbstractVectorExpr
+{
+	public SPEmptySetMatching()
+	{
+		System.out.println("an empty vector matching");
+	}
+	
+	public String toString()
+	{
+		return "[]";
+	}
+}
---
+++ languages/Natlab/src/natlab/tame/builtin/shapeprop/ast/SPPlusFunCall.java
@@ -0,0 +1,18 @@
+package natlab.tame.builtin.shapeprop.ast;
+
+public class SPPlusFunCall extends SPAbstractVertcatExprArg
+{
+	String l;
+	SPAbstractPattern f;
+	public SPPlusFunCall(String l, SPAbstractPattern f)
+	{
+		this.l = l;
+		this.f = f;
+		System.out.println("a plus of lowercase and fnCall");
+	}
+	
+	public String toString()
+	{
+		return l.toString()+"+"+f.toString();
+	}
+}
