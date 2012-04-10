6c502097d1ab3c3933689d4f9446d41403b8e4ac
Anton Dubrau <ant6nd@gmail.com>
2012-04-10 01:22:24 +0000

    comments
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2105 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@d93894d179ad41bc3e87a0182c410c617d230cda

--- languages/Natlab/src/natlab/tame/valueanalysis/aggrvalue/MatrixPropagator.java
+++ languages/Natlab/src/natlab/tame/valueanalysis/aggrvalue/MatrixPropagator.java
@@ -2,6 +2,17 @@ package natlab.tame.valueanalysis.aggrvalue;
 
 import natlab.tame.valueanalysis.value.*;
 
+/**
+ * In order to use aggr value together with a new matrix value abstraction to extend
+ * the value analysis, one has to provide a new matrix propagator. This is a value
+ * propagator where it is assumed that any incoming arguments are of type D, i.e.
+ * they are matrices. This is enforced by the AggrValuePropagator.
+ * 
+ * Any analysis based on AggrValues uses an AggrValuePropagator. At its base case,
+ * the MatrixPropagator is called to deal with builtins called with matrices.
+ */
+
+
 abstract public class MatrixPropagator<D extends MatrixValue<D>> extends ValuePropagator<AggrValue<D>> {
 	protected AggrValueFactory<D> factory;
 	
