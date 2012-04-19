dc73ea06eb27fb8b53c011aa9fc3f4a059f93efe
Xu Li <aaronleemcgill@gmail.com>
2012-04-19 16:53:19 +0000

    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2115 89b902b4-d83f-0410-b942-89f7a419ffdb
    Migrated from Sable/mclab@b5b399427dfac60529e3906903c79d282a2e97a9

--- languages/Natlab/src/natlab/tame/valueanalysis/basicmatrix/BasicMatrixValuePropagator.java
+++ languages/Natlab/src/natlab/tame/valueanalysis/basicmatrix/BasicMatrixValuePropagator.java
@@ -1,6 +1,7 @@
 package natlab.tame.valueanalysis.basicmatrix;
 
 import java.util.*;
+
 import natlab.tame.builtin.*;
 import natlab.tame.builtin.classprop.*;
 import natlab.tame.classes.reference.*;
@@ -28,41 +29,31 @@ public class BasicMatrixValuePropagator extends MatrixPropagator<BasicMatrixValu
     public Res<AggrValue<BasicMatrixValue>> caseBuiltin(Builtin builtin,
             Args<AggrValue<BasicMatrixValue>> arg) {
         //deal with constants
-        if (arg.isAllConstant()){ //FIXME
-            Constant cResult = builtin.visit(constantProp, arg);
-            if (cResult != null){
-                return Res.<AggrValue<BasicMatrixValue>>newInstance(new BasicMatrixValue(cResult));
-            }
+    	Constant cResult = builtin.visit(constantProp, arg);
+    	if (cResult != null){
+    		return Res.<AggrValue<BasicMatrixValue>>newInstance(new BasicMatrixValue(cResult));
+    	}
+    	
+    	//if the result is not a constant, just do mclass propagation
+        List<Set<ClassReference>> matchResult = 
+                builtin.visit(ClassPropagator.<AggrValue<BasicMatrixValue>>getInstance(),arg);
+        if (matchResult == null){ //class prop returned error
+            return Res.newErrorResult(builtin.getName()+" is not defined for arguments "+arg);
         }
-                
-        //if it's not constant check whether the Builtin implements class propagation
-        if (builtin instanceof HasClassPropagationInfo){
-            if (DEBUG) System.out.println("case builtin: "+builtin
-                    +" prop: "+((HasClassPropagationInfo)builtin).getMatlabClassPropagationInfo()
-                    +" args: "+arg);
-            List<HashSet<ClassReference>> matchResult =                                          //XU changed here
-            		builtin.visit(ClassPropagator.<AggrValue<BasicMatrixValue>>getInstance(),arg);
-            if (matchResult == null){
-                return Res.newErrorResult(builtin.getName()+" is not defined for arguments "+arg);
-            }
-            return matchResultToRes(matchResult);
-        }
-        //else throw an error
-        throw new UnsupportedOperationException("No class propagation defined for builtin "+builtin);
+    	
+        //build results out of the result classes
+        return matchResultToRes(matchResult);
         
         //deal with shape
         
         //deal with complex
     }
     
-    private Res<AggrValue<BasicMatrixValue>> matchResultToRes(List<HashSet<ClassReference>> matchResult){
+    private Res<AggrValue<BasicMatrixValue>> matchResultToRes(List<Set<ClassReference>> matchResult){
         //go through and fill in result
         Res<AggrValue<BasicMatrixValue>> result = Res.newInstance();
-        for (HashSet<ClassReference> values: matchResult){
+        for (Set<ClassReference> values: matchResult){
             HashMap<ClassReference,AggrValue<BasicMatrixValue>> map = new HashMap<ClassReference,AggrValue<BasicMatrixValue>>();
-            if (values.size() == 0){
-                throw new UnsupportedOperationException("encountered match result with 0 values");
-            }
             for (ClassReference classRef : values){
                 map.put(classRef,new BasicMatrixValue((PrimitiveClassReference)classRef));
             }
