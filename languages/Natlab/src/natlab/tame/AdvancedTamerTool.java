9380fb2b678db099ff290391e3022fabc847b78f
Vineet Kumar <vineet7kumar@gmail.com>
2012-06-01 04:19:07 +0000

    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2195 89b902b4-d83f-0410-b942-89f7a419ffdb
    Migrated from Sable/mclab@19a8b466a301c1f3657d16d9040f3a90f067e077

---
+++ languages/Natlab/src/natlab/tame/AdvancedTamerTool.java
@@ -0,0 +1,55 @@
+package natlab.tame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import natlab.tame.builtin.Builtin;
+import natlab.tame.callgraph.FunctionCollection;
+import natlab.tame.callgraph.StaticFunction;
+import natlab.tame.classes.reference.PrimitiveClassReference;
+import natlab.tame.valueanalysis.IntraproceduralValueAnalysis;
+import natlab.tame.valueanalysis.advancedMatrix.AdvancedMatrixValue;
+import natlab.tame.valueanalysis.advancedMatrix.AdvancedMatrixValueFactory;
+import natlab.tame.valueanalysis.aggrvalue.AggrValue;
+import natlab.tame.valueanalysis.basicmatrix.*;
+import natlab.tame.valueanalysis.value.Args;
+import natlab.toolkits.filehandling.genericFile.GenericFile;
+import natlab.toolkits.path.FilePathEnvironment;
+
+public class AdvancedTamerTool {
+	public IntraproceduralValueAnalysis<AggrValue<AdvancedMatrixValue>> 
+    tameMatlabToSingleFunction(java.io.File mainFile, List<AggrValue<AdvancedMatrixValue>> inputValues){
+		GenericFile gFile = GenericFile.create(mainFile); //file -> generic file
+		FilePathEnvironment path = new FilePathEnvironment(gFile, Builtin.getBuiltinQuery()); //get path environment obj
+		FunctionCollection callgraph = new FunctionCollection(path); //build simple callgraph
+		StaticFunction function = callgraph.getAsInlinedStaticFunction(); //inline all
+
+
+		@SuppressWarnings("unchecked")
+		IntraproceduralValueAnalysis<AggrValue<AdvancedMatrixValue>> analysis = 
+        new IntraproceduralValueAnalysis<AggrValue<AdvancedMatrixValue>>(
+        		null, function, new AdvancedMatrixValueFactory(), 
+        		Args.<AggrValue<AdvancedMatrixValue>>newInstance(inputValues));
+		//System.out.println("before analyze!");
+		analysis.analyze(); //run analysis
+		return analysis;
+}
+
+/**
+* This is the same as tameMatlabToSingleFunction, but takes 
+* a list of PrimitiveClassReferences as arguments, rather than
+* abstract values. PrimitiveClassReference is an enum of 
+* the builtin matlab classes representing matrizes.
+*/
+
+	public IntraproceduralValueAnalysis<AggrValue<AdvancedMatrixValue>> 
+	tameMatlabToSingleFunctionFromClassReferences(java.io.File mainFile, List<PrimitiveClassReference> inputValues){
+//System.out.println(inputValues);
+		AdvancedMatrixValueFactory factory = new AdvancedMatrixValueFactory();
+		ArrayList<AggrValue<AdvancedMatrixValue>> list = new ArrayList<AggrValue<AdvancedMatrixValue>>(inputValues.size());
+		for (PrimitiveClassReference ref : inputValues){
+			list.add(new AdvancedMatrixValue(ref));         //here, we create list of basicMatrixValue with the input default class (double)
+		}
+		return tameMatlabToSingleFunction(mainFile, list);
+	}
+}
