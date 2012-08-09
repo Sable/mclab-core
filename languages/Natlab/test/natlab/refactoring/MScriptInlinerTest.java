3dd2df6cbc2baf30e8e80dbdccb999b80f428655
Soroush Radpour <sradpo@cs.mcgill.ca>
2012-08-09 10:15:53 +0000

    First batch of refactoring tests
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2235 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@ad8942ccb218b31c3690a3be47522fd5896c634b

---
+++ languages/Natlab/test/natlab/refactoring/MScriptInlinerTest.java
@@ -0,0 +1,92 @@
+package natlab.refactoring;
+
+import ast.*;
+import natlab.*;
+import natlab.toolkits.filehandling.genericFile.*;
+
+import junit.framework.*;
+
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+public class MScriptInlinerTest extends TestCase {
+    CompilationUnits cu = null;
+    String[] files = null;
+    BuiltinFile root = null;
+
+    private Program addFile(String filename, String content) {
+        ArrayList<CompilationProblem> errorList = new ArrayList<CompilationProblem>();
+        Program p = Parse.parseFile(filename, new StringReader(content), errorList);
+        assertTrue(errorList.isEmpty());
+        p.setFile(root.getBuiltin(filename));
+        cu.addProgram(p);
+        return p;
+    }
+
+    public void testNoInline() {
+        cu = new CompilationUnits();
+        files = new String[]{"/s1.m", "f1.m"};
+        root = new BuiltinFile(files);
+        addFile("/s1.m", "f = 1;");
+        Program p = addFile("/f1.m", "function x() \n f = 2; \n end");
+        MScriptInliner scriptInliner = new MScriptInliner(cu);
+        assertTrue(scriptInliner.inlineAll().isEmpty());
+        assertEquals(p.getPrettyPrinted(), "function  [] = x()\n  f = 2;\nend\n");      
+    }
+
+    public void testInlineNormal() {
+        cu = new CompilationUnits();
+        files = new String[]{"/s1.m", "/f1.m"};
+        root = new BuiltinFile(files);
+        addFile("/s1.m", "f = 1;");
+        cu.setRootFolder(root);
+        Program p = addFile("/f1.m", "function x() \n s1; \n end");
+        MScriptInliner scriptInliner = new MScriptInliner(cu);
+        LinkedList<LinkedList<Exception>> errors = scriptInliner.inlineAll();
+        assertEquals(errors.size(), 1);
+        assertTrue(errors.get(0).isEmpty());
+        assertEquals(p.getPrettyPrinted(), "function  [] = x()\n  f = 1;\nend\n");      
+    }
+
+    public void testInlineFunctionVarConflict() {
+        cu = new CompilationUnits();
+        files = new String[]{"/s1.m", "/f1.m"};
+        root = new BuiltinFile(files);
+        addFile("/s1.m", "b = @size;");
+        cu.setRootFolder(root);
+        Program p = addFile("/f1.m", "function x() \n s1; size = 1; \n end");
+        MScriptInliner scriptInliner = new MScriptInliner(cu);
+        LinkedList<LinkedList<Exception>> errors = scriptInliner.inlineAll();
+        assertEquals(errors.size(), 1);
+        assertEquals(errors.get(0).get(0).getClass(), Exceptions.RenameRequired.class);
+    }
+
+    public void testUnassignedIDBecomesVariable() {
+        cu = new CompilationUnits();
+        files = new String[]{"/s1.m", "/f1.m", "/zeros.m"};
+        root = new BuiltinFile(files);
+        addFile("/s1.m", "b = zeros(1,2);");
+        cu.setRootFolder(root);
+        Program p = addFile("/f1.m", "function x() \n s1; zeros = 1; \n end");
+        MScriptInliner scriptInliner = new MScriptInliner(cu);
+        LinkedList<LinkedList<Exception>> errors = scriptInliner.inlineAll();
+        assertEquals(errors.size(), 1);
+        assertEquals(errors.get(0).get(0).getClass(), Exceptions.UnassignedVariableException.class);
+    }
+
+    public void testIDBecomesFunction() {
+        cu = new CompilationUnits();
+        files = new String[]{"/s1.m", "/f1.m", "/zeros.m"};
+        root = new BuiltinFile(files);
+        addFile("/s1.m", "b = zeros(1,2);");
+        cu.setRootFolder(root);
+        Program p = addFile("/f1.m", "function x() \n s1; \n end");
+        MScriptInliner scriptInliner = new MScriptInliner(cu);
+        LinkedList<LinkedList<Exception>> errors = scriptInliner.inlineAll();
+        assertEquals(errors.size(), 1);
+        assertEquals(errors.get(0).get(0).getClass(), Exceptions.WarnIDToFunException.class);
+    }
+}
