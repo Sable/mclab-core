50dc274b28710002293e10f96347185de7c2e56b
Vineet Kumar <vineet7kumar@gmail.com>
2012-10-17 01:11:13 +0000

    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2357 89b902b4-d83f-0410-b942-89f7a419ffdb
    Migrated from Sable/mclab@f33abd461f5d13c48cac15fe3fe60ce91245ebd9

--- languages/Natlab/MiX10.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar /home/2011/vkumar5/mclab/Project/languages/Natlab/MiX10.jar $*
--- languages/Natlab/natlab.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar /home/2011/xli141/Project/languages/Natlab/Natlab.jar $*
