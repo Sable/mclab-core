cdc1507853d0c24b83181589c86ed6d01af154b7
Ismail Badawi <isbadawi@gmail.com>
2012-10-20 16:42:34 +0000

    More clean up; replace duplicate code in ASTToolBox with calls to NodeFinder
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2363 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@7bd9aa7bc074a4382a598032dcaf4b3eec389dc1

--- languages/Natlab/src/natlab/ASTToolBox.java
+++ languages/Natlab/src/natlab/ASTToolBox.java
@@ -21,144 +21,88 @@
 
 package natlab;
 
-import ast.*;
+import natlab.toolkits.utils.NodeFinder;
+import ast.ASTNode;
+import ast.AssignStmt;
+import ast.ForStmt;
+import ast.Function;
+import ast.IfStmt;
+import ast.ParameterizedExpr;
+import ast.Program;
+import ast.Script;
+import ast.Stmt;
+import ast.WhileStmt;
 
 public class ASTToolBox {
+  public static Stmt getParentStmtNode(ASTNode varNode) {
+    return NodeFinder.findParent(varNode, Stmt.class);
+  }
 
-	// Find the close parent node, whose parent is a statment list  
- 	public static Stmt getParentStmtNode(ASTNode varNode) {
-	    ASTNode parentNode = varNode.getParent();
-	    do{
-		    if((varNode instanceof Stmt)) {
-		    	parentNode = varNode;
-		    } else {
-			    // varNode/e.getNodeLocation() is NameExpr, it may directly belongs to
-		    	// AssignStmt, or ParameterizedExpr.
-		    	while ((parentNode!=null) && !(parentNode instanceof Stmt)) {
-		    		varNode = parentNode;
-		    		parentNode = parentNode.getParent(); 	   
-		    	}
-		    }
-	    } while((parentNode!=null) && !(parentNode instanceof Stmt));
-	    
-	    return (Stmt) parentNode;
- 	}
+  public static AssignStmt getParentAssignStmtNode(ASTNode varNode) {
+    return NodeFinder.findParent(varNode,  AssignStmt.class);
+  }
 
-	// Find the close parent node 
- 	public static AssignStmt getParentAssignStmtNode(ASTNode varNode) {
-	    ASTNode parentNode = varNode.getParent();
-	    // If the varNode is already the assignment-statement, then don't need 
-	    // to find it's parent
-	    if((varNode instanceof AssignStmt)) {
-	    	parentNode = varNode;
-	    } else {
-		    // varNode/e.getNodeLocation() is NameExpr, it may directly belongs to
-	    	// AssignStmt, or ParameterizedExpr.
-	    	while ((parentNode!=null) && !(parentNode instanceof AssignStmt)) {
-	    		varNode = parentNode;
-	    		parentNode = parentNode.getParent(); 	   
-	    	}
-	    }
-	    return (AssignStmt)parentNode;
- 	}
-	// Find the close parent node 
- 	public static ast.List getParentStmtListNode(ASTNode varNode) {
-	    ASTNode parentNode = varNode.getParent();
-	    // If the varNode is already the assignment-statement, then don't need 
-	    // to find it's parent
-	    if((varNode instanceof ast.List)) {
-	    	parentNode = varNode;
-	    } else {
-		    // varNode/e.getNodeLocation() is NameExpr, it may directly belongs to
-	    	// AssignStmt, or ParameterizedExpr.
-	    	while ((parentNode!=null) && !(parentNode instanceof ast.List)) {
-	    		varNode = parentNode;
-	    		parentNode = parentNode.getParent(); 	   
-	    	}
-	    }
-	    return (ast.List) parentNode;
- 	}
-	
- 	public static boolean isInsideArray(ASTNode varNode) {
- 		boolean bResult = false;
-	    ASTNode parentNode = varNode;
-	    do{
-		    if((parentNode instanceof AssignStmt) ) {
-		    	break;
-		    } if((parentNode instanceof ParameterizedExpr) ) {
-		    	bResult = true;
-		    	break;
-		    } else {
-	    		parentNode = parentNode.getParent(); 	   
-		    }
-	    } while((parentNode!=null) && !bResult);
-	    
-	    return bResult;
- 	}
+  public static ast.List getParentStmtListNode(ASTNode varNode) {
+    return NodeFinder.findParent(varNode, ast.List.class);
+  }
 
- 	public static boolean isInsideLoop(ASTNode varNode) {
- 		boolean bResult = false;
-	    ASTNode parentNode = varNode;
-	    do{
-		    if((parentNode instanceof WhileStmt) 
-		    		|| (parentNode instanceof ForStmt)) {
-		    	bResult = true;
-		    	break;
-		    } else {
-	    		parentNode = parentNode.getParent(); 	   
-		    }
-	    } while((parentNode!=null) && !(parentNode instanceof Program) && !bResult);
-	    
-	    return bResult;
- 	}
- 	public static ForStmt getParentForStmtNode(ASTNode varNode) {
-	    ASTNode parentNode = varNode.getParent();
-	    // If the varNode is already the assignment-statement, then don't need 
-	    // to find it's parent
-	    if((varNode instanceof ForStmt)) {
-	    	parentNode = varNode;
-	    } else {
-		    // varNode/e.getNodeLocation() is NameExpr, it may directly belongs to
-	    	// AssignStmt, or ParameterizedExpr.
-	    	while ((parentNode!=null) && !(parentNode instanceof ForStmt)) {
-	    		varNode = parentNode;
-	    		parentNode = parentNode.getParent(); 	   
-	    	}
-	    }
-	    return (ForStmt)parentNode;
- 	}
+  public static ForStmt getParentForStmtNode(ASTNode varNode) {
+    return NodeFinder.findParent(varNode, ForStmt.class);
+  }
 
- 	public static IfStmt getParentIfStmtNode(ASTNode varNode) {
-	    ASTNode parentNode = varNode.getParent();
-	    // If the varNode is already the assignment-statement, then don't need 
-	    // to find it's parent
-	    if((varNode instanceof IfStmt)) {
-	    	parentNode = varNode;
-	    } else {
-		    // varNode/e.getNodeLocation() is NameExpr, it may directly belongs to
-	    	// AssignStmt, or ParameterizedExpr.
-	    	while ((parentNode!=null) && !(parentNode instanceof IfStmt)) {
-	    		varNode = parentNode;
-	    		parentNode = parentNode.getParent(); 	   
-	    	}
-	    }
-	    return (IfStmt)parentNode;
- 	}
- 	public static ASTNode getSubtreeRoot(ASTNode varNode) {
-	    ASTNode parentNode = varNode.getParent();
-	    // If the varNode is already the assignment-statement, then don't need 
-	    // to find it's parent
-	    if((varNode instanceof Script)|| (varNode instanceof Function)) {
-	    	parentNode = varNode;
-	    } else {
-		    // varNode/e.getNodeLocation() is NameExpr, it may directly belongs to
-	    	// AssignStmt, or ParameterizedExpr.
-	    	while ((parentNode!=null) && 
-	    			!((parentNode instanceof Script)||(parentNode instanceof Function))) {
-	    		varNode = parentNode;
-	    		parentNode = parentNode.getParent(); 	   
-	    	}
-	    }
-	    return parentNode;
- 	}
+  public static IfStmt getParentIfStmtNode(ASTNode varNode) {
+    return NodeFinder.findParent(varNode, IfStmt.class);
+  }
+
+  public static boolean isInsideArray(ASTNode varNode) {
+    boolean bResult = false;
+    ASTNode parentNode = varNode;
+    do{
+      if((parentNode instanceof AssignStmt) ) {
+        break;
+      } if((parentNode instanceof ParameterizedExpr) ) {
+        bResult = true;
+        break;
+      } else {
+        parentNode = parentNode.getParent(); 	   
+      }
+    } while((parentNode!=null) && !bResult);
+
+    return bResult;
+  }
+  
+
+  public static boolean isInsideLoop(ASTNode varNode) {
+    boolean bResult = false;
+    ASTNode parentNode = varNode;
+    do{
+      if((parentNode instanceof WhileStmt) 
+          || (parentNode instanceof ForStmt)) {
+        bResult = true;
+        break;
+      } else {
+        parentNode = parentNode.getParent(); 	   
+      }
+    } while((parentNode!=null) && !(parentNode instanceof Program) && !bResult);
+
+    return bResult;
+  }
+
+  public static ASTNode getSubtreeRoot(ASTNode varNode) {
+    ASTNode parentNode = varNode.getParent();
+    // If the varNode is already the assignment-statement, then don't need 
+    // to find it's parent
+    if((varNode instanceof Script)|| (varNode instanceof Function)) {
+      parentNode = varNode;
+    } else {
+      // varNode/e.getNodeLocation() is NameExpr, it may directly belongs to
+      // AssignStmt, or ParameterizedExpr.
+      while ((parentNode!=null) && 
+          !((parentNode instanceof Script)||(parentNode instanceof Function))) {
+        varNode = parentNode;
+        parentNode = parentNode.getParent(); 	   
+      }
+    }
+    return parentNode;
+  }
 }
--- languages/Natlab/src/natlab/toolkits/utils/NodeFinder.java
+++ languages/Natlab/src/natlab/toolkits/utils/NodeFinder.java
@@ -1,67 +1,69 @@
 package natlab.toolkits.utils;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import natlab.refactoring.AbstractNodeFunction;
 import nodecases.AbstractNodeCaseHandler;
-import natlab.toolkits.path.LookupResult.TYPE;
 import ast.ASTNode;
 import ast.EmptyStmt;
 
-public class NodeFinder{
-	
-	public static <T extends ASTNode> List<T> find(ASTNode n, Class<T> type){
-		final Class<T> t= type;
-		final List<T> res = new LinkedList<T>();
-		new AbstractNodeCaseHandler() {
-			public void caseASTNode(ASTNode n) {
-				if (t.isInstance(n)) {
-					res.add((T) n);
-				}
-				for (int i = 0; i < n.getNumChild(); i++) {
-					caseASTNode(n.getChild(i));
-				}
-			}
-		}.caseASTNode(n);
-		return res;
-	}
+public class NodeFinder {
+  public static <T extends ASTNode> List<T> find(ASTNode n, final Class<T> type) {
+    final List<T> res = new LinkedList<T>();
+    new AbstractNodeCaseHandler() {
+      public void caseASTNode(ASTNode n) {
+        if (type.isInstance(n)) {
+          res.add(type.cast(n));
+        }
+        for (int i = 0; i < n.getNumChild(); i++) {
+          caseASTNode(n.getChild(i));
+        }
+      }
+    }.caseASTNode(n);
+    return res;
+  }
 
-	public static <T extends ASTNode> T findParent(ASTNode n, Class<T> type){
-		while (n!=null && (!type.isInstance(n)) ){
-			n = n.getParent();
-		}
-		return (T) n;
-	}
-	
-	public static <T extends ASTNode> void apply(final ASTNode n, Class<T> type, final AbstractNodeFunction<T> func){
-		final Class<T> t= type;
-		new AbstractNodeCaseHandler() {
-			public void caseASTNode(ASTNode n) {
-				if (t.isInstance(n)) 
-					func.apply((T)n);
-				for (int i = 0; i < n.getNumChild(); i++) {
-					caseASTNode(n.getChild(i));
-				}
-			}
-		}.caseASTNode(n);
-	}
-    public static Map<ASTNode, Integer> nodeCount(ASTNode t){
-        final Map<ASTNode, Integer> res = new HashMap<ASTNode, Integer>();
-		new AbstractNodeCaseHandler() {
-            int count = 0;
-			public void caseASTNode(ASTNode n) {
-                int before = count;
-                if (n instanceof EmptyStmt){
-                    res.put(n, 0);
-                    return;
-                }
-                count++;
-				for (int i = 0; i < n.getNumChild(); i++) {
-					caseASTNode(n.getChild(i));
-				}
-                res.put(n, count-before);
-			}
-		}.caseASTNode(t);
-        return res;
+  public static <T extends ASTNode> T findParent(ASTNode n, Class<T> type) {
+    while (n != null && (!type.isInstance(n))) {
+      n = n.getParent();
     }
+    return type.cast(n);
+  }
+
+  public static <T extends ASTNode> void apply(final ASTNode n, final Class<T> type,
+      final AbstractNodeFunction<T> func) {
+    new AbstractNodeCaseHandler() {
+      public void caseASTNode(ASTNode n) {
+        if (type.isInstance(n)) {
+          func.apply(type.cast(n));
+        }
+        for (int i = 0; i < n.getNumChild(); i++) {
+          caseASTNode(n.getChild(i));
+        }
+      }
+    }.caseASTNode(n);
+  }
+
+  public static Map<ASTNode, Integer> nodeCount(ASTNode t) {
+    final Map<ASTNode, Integer> res = new HashMap<ASTNode, Integer>();
+    new AbstractNodeCaseHandler() {
+      int count = 0;
+      public void caseASTNode(ASTNode n) {
+        int before = count;
+        if (n instanceof EmptyStmt) {
+          res.put(n, 0);
+          return;
+        }
+        count++;
+        for (int i = 0; i < n.getNumChild(); i++) {
+          caseASTNode(n.getChild(i));
+        }
+        res.put(n, count - before);
+      }
+    }.caseASTNode(t);
+    return res;
+  }
 }
