9f21ab83576e4934f132d67f753bc794425a4834
Ismail Badawi <isbadawi@gmail.com>
2012-10-31 15:46:06 +0000

    Some more referencing removed code
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2406 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@041594eea23cc4bfde9a80ee883f9cc1a0f9049c

--- languages/Natlab/test/natlab/AllTests.java
+++ languages/Natlab/test/natlab/AllTests.java
@@ -6,41 +6,38 @@ import natlab.refactoring.*;
 
 /** Top-level test suite.  Contains all tests. */
 public class AllTests {
+  public static Test suite() {
+    TestSuite suite = new TestSuite();
+    //$JUnit-BEGIN$
+    suite.addTestSuite(NatlabScannerTests.class);
+    //TODO-AC: uncomment
+    suite.addTestSuite(NatlabParserPassTests.class);
+    suite.addTestSuite(NatlabParserFailTests.class);
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite();
-		//$JUnit-BEGIN$
-		suite.addTestSuite(NatlabScannerTests.class);
-		//TODO-AC: uncomment
-		suite.addTestSuite(NatlabParserPassTests.class);
-		suite.addTestSuite(NatlabParserFailTests.class);
+    suite.addTestSuite(SymbolTablePassTests.class);
+    suite.addTestSuite(SymbolTableFailTests.class);
+    suite.addTestSuite(SymbolTableSanityTests.class);
 
-		suite.addTestSuite(SymbolTablePassTests.class);
-		suite.addTestSuite(SymbolTableFailTests.class);
-		suite.addTestSuite(SymbolTableSanityTests.class);
+    //suite.addTestSuite(LookupLValuePassTests.class);
+    //suite.addTestSuite(LookupLValueFailTests.class);
+    //suite.addTestSuite(LookupLValueSanityTests.class);
 
-		//suite.addTestSuite(LookupLValuePassTests.class);
-		//suite.addTestSuite(LookupLValueFailTests.class);
-		//suite.addTestSuite(LookupLValueSanityTests.class);
+    suite.addTestSuite(ErrorCheckTest.class);
 
-		suite.addTestSuite(ErrorCheckTest.class);
-		suite.addTestSuite(XMLCommandPassTests.class);
+    //rewrite tests
+    suite.addTestSuite(TransformedNodeTests.class);
 
-                //rewrite tests
-                suite.addTestSuite(TransformedNodeTests.class);
+    //suite.addTestSuite(MultireturnSimplifyPassTests.class);
+    //suite.addTestSuite(LeftThreeAddressSimplifyPassTests.class);
+    //suite.addTestSuite(SimpleIfPassTests.class);
+    suite.addTestSuite(MultiRewritePassTests.class);
 
-                //suite.addTestSuite(MultireturnSimplifyPassTests.class);
-                //suite.addTestSuite(LeftThreeAddressSimplifyPassTests.class);
-                //suite.addTestSuite(SimpleIfPassTests.class);
-                suite.addTestSuite(MultiRewritePassTests.class);
+    suite.addTestSuite(MScriptInlinerTest.class);
+    suite.addTestSuite(FEvalToCallTest.class);
+    suite.addTestSuite(FunctionInlinerTest.class);
+    suite.addTestSuite(ExtractFunctionTest.class);
 
-        suite.addTestSuite(MScriptInlinerTest.class);
-        suite.addTestSuite(FEvalToCallTest.class);
-        suite.addTestSuite(FunctionInlinerTest.class);
-        suite.addTestSuite(ExtractFunctionTest.class);
-
-		//$JUnit-END$
-		return suite;
-	}
-
+    //$JUnit-END$
+    return suite;
+  }
 }
