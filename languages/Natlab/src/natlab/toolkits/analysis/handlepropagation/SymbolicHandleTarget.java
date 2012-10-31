e74d2ec321fb7fed3565f586fb8b44cceeac541a
Ismail Badawi <isbadawi@gmail.com>
2012-10-31 22:29:53 +0000

    Remove empty file
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2419 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@3b69d607fb6439d75daf85a2efba3418a1d7c736

--- languages/Natlab/src/natlab/toolkits/analysis/handlepropagation/SymbolicHandleTarget.java
+++
@@ -1,6 +0,0 @@
-package natlab.toolkits.analysis.handlepropagation;
-
-/**
- * Represents handle targets that are not concretely known. These
- * include targets that come from function arguments. 
- */
