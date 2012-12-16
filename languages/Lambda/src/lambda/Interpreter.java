e6de0ac0109ef81a30dd4a8dc1773b9b7df94a5d
Ismail Badawi <isbadawi@gmail.com>
2012-12-16 22:42:17 +0000

    Add includeantruntime=false to javac ant task to suppress warnings
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2475 89b902b4-d83f-0410-b942-89f7a419ffdb
    
    Migrated from Sable/mclab@a4eafb56d099e7a40fafd8e49977cba146508786

--- languages/Lambda/src/lambda/Interpreter.java
+++ languages/Lambda/src/lambda/Interpreter.java
@@ -27,7 +27,7 @@ public class Interpreter {
 				LambdaScanner scanner = new LambdaScanner(new StringReader(line));
 				LambdaParser parser = new LambdaParser();
 				Root original = (Root) parser.parse(scanner);
-				SPNode reduced = original.reduce();
+				Node reduced = original.reduce();
 
 				System.out.println("Before: " + original.getStructureString());
 				System.out.println("After:  " + reduced.getStructureString());
