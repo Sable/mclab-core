7cf67d43187e2422a218d38df097055721457954
Ismail Badawi <isbadawi@gmail.com>
2013-01-15 14:15:17 -0500

    Clean up ReachingDefs a little
    Migrated from Sable/mclab@52f9a6ce94663565b017e3b949600e218e815502

--- languages/Natlab/src/natlab/toolkits/analysis/test/ReachingDefs.java
+++ languages/Natlab/src/natlab/toolkits/analysis/test/ReachingDefs.java
@@ -21,6 +21,7 @@ package natlab.toolkits.analysis.test;
 import java.util.HashSet;
 import java.util.Set;
 
+import natlab.toolkits.analysis.AbstractFlowMap;
 import natlab.toolkits.analysis.HashMapFlowMap;
 import natlab.toolkits.analysis.Merger;
 import natlab.toolkits.analysis.Mergers;
@@ -33,138 +34,119 @@ import ast.Name;
 import ast.Script;
 import ast.Stmt;
 
+import com.google.common.collect.Sets;
+
 /**
- * A simple forward analysis example, computing reaching definitions. 
- *
+ * A simple forward analysis example, computing reaching definitions.
+ * 
  * @author Jesse Doherty
  */
-public class ReachingDefs 
-    extends AbstractSimpleStructuralForwardAnalysis<HashMapFlowMap<String,Set<AssignStmt>>>
-{
-	public static final AssignStmt UNDEF = new AssignStmt();
-	public static final AssignStmt PARAM = new AssignStmt();
-	public static final AssignStmt GLOBAL = new AssignStmt();
+public class ReachingDefs extends
+        AbstractSimpleStructuralForwardAnalysis<HashMapFlowMap<String, Set<AssignStmt>>> {
+  public static final AssignStmt UNDEF = new AssignStmt();
+  public static final AssignStmt PARAM = new AssignStmt();
+  public static final AssignStmt GLOBAL = new AssignStmt();
 
-	private Merger<Set<AssignStmt>> merger = Mergers.union();
+  private final Merger<Set<AssignStmt>> merger = Mergers.union();
 
-    private HashMapFlowMap<String,Set<AssignStmt>> startMap;
-    private NameCollector nameCollector;
+  private final HashMapFlowMap<String, Set<AssignStmt>> startMap;
+  private final NameCollector nameCollector;
 
-    public ReachingDefs( ASTNode<?> tree )
-    {
-        super(tree);
-//        DEBUG=true;
-        startMap = new HashMapFlowMap<String,Set<AssignStmt>>(merger);
-        nameCollector= new NameCollector(tree);
-        nameCollector.analyze();
-        for( String var : nameCollector.getAllNames() )
-            startMap.put( var, new HashSet<AssignStmt>() );
+  public ReachingDefs(ASTNode<?> tree) {
+    super(tree);
+    startMap = new HashMapFlowMap<String, Set<AssignStmt>>(merger);
+    nameCollector = new NameCollector(tree);
+    nameCollector.analyze();
+    for (String var : nameCollector.getAllNames())
+      startMap.put(var, new HashSet<AssignStmt>());
+  }
+
+  /**
+   * Defines the merge operation for this analysis.
+   * 
+   * This implementation uses the union method defined by
+   * {@link AbstractFlowMap}. Note that the union method deals with the cases
+   * where {@literal in1==in2}, {@literal in1==out} or {@literal in2==out}.
+   */
+  @Override
+  public void merge(HashMapFlowMap<String, Set<AssignStmt>> in1,
+          HashMapFlowMap<String, Set<AssignStmt>> in2, HashMapFlowMap<String, Set<AssignStmt>> out) {
+    Set<String> keys = Sets.newHashSet(in1.keySet());
+    keys.addAll(in2.keySet());
+    for (String s : keys) {
+      Set<AssignStmt> res = new HashSet<AssignStmt>();
+      res.addAll(in1.get(s));
+      res.addAll(in2.get(s));
+      out.put(s, res);
     }
-    
-    /**
-     * Defines the merge operation for this analysis.
-     *
-     * This implementation uses the union method defined by {@link
-     * AbstrcatFlowMap}. Note that the union method deals with the
-     * cases where {@literal in1==in2}, {@literal in1==out} or
-     * {@literal in2==out}. 
-     */
-    public void merge( HashMapFlowMap<String,Set<AssignStmt>> in1,
-                       HashMapFlowMap<String,Set<AssignStmt>> in2,
-                       HashMapFlowMap<String,Set<AssignStmt>> out )
-    {
-    	Set<String> keys = new HashSet<String>(in1.keySet());
-        keys.addAll(in2.keySet());
-        for (String s: keys){
-        	Set<AssignStmt> res = new HashSet<AssignStmt>();
-    		res.addAll(in1.get(s));
-    		res.addAll(in2.get(s));
-        	out.put(s, res);
-        }
+  }
+
+  /**
+   * Creates a copy of the FlowMap with copies of the contained set.
+   */
+  @Override
+  public void copy(HashMapFlowMap<String, Set<AssignStmt>> in,
+          HashMapFlowMap<String, Set<AssignStmt>> out) {
+    if (in == out)
+      return;
+    out.clear();
+    for (String i : in.keySet())
+      out.put(i, Sets.newHashSet(in.get(i)));
+  }
+
+  /**
+   * Creates a copy of the given flow-map and returns it.
+   */
+  public HashMapFlowMap<String, Set<AssignStmt>> copy(HashMapFlowMap<String, Set<AssignStmt>> in) {
+    HashMapFlowMap<String, Set<AssignStmt>> out = new HashMapFlowMap<String, Set<AssignStmt>>();
+    copy(in, out);
+    return out;
+  }
+
+  @Override
+  public void caseFunction(Function f) {
+    currentOutSet = newInitialFlow();
+    currentInSet = currentOutSet;
+    for (Name n : NodeFinder.of(Name.class).findIn(f)) {
+      currentOutSet.put(n.getID(), Sets.<AssignStmt> newHashSet(UNDEF));
     }
-
-    /** 
-     * Creates a copy of the FlowMap with copies of the contained set.
-     */
-    public void copy( HashMapFlowMap<String,Set<AssignStmt>> in,
-                      HashMapFlowMap<String,Set<AssignStmt>> out )
-    {
-        if( in == out )
-            return;
-        out.clear();
-        for( String i : in.keySet() )
-            out.put(i, new HashSet<AssignStmt>(in.get(i)));
+    for (Name inputArg : f.getInputParamList()) {
+      currentOutSet.put(inputArg.getID(), Sets.<AssignStmt> newHashSet(PARAM));
     }
+    caseASTNode(f.getStmts());
+    outFlowSets.put(f, currentOutSet);
+  }
 
-    /**
-     * Creates a copy of the given flow-map and returns it.
-     */
-    public HashMapFlowMap<String,Set<AssignStmt>> 
-        copy( HashMapFlowMap<String,Set<AssignStmt>> in )
-    {
-        HashMapFlowMap<String,Set<AssignStmt>> out = new HashMapFlowMap<String,Set<AssignStmt>>();
-        copy(in, out);
-        return out;
+  @Override
+  public void caseScript(Script f) {
+    currentOutSet = newInitialFlow();
+    currentInSet = currentOutSet;
+    for (Name n : NodeFinder.of(Name.class).findIn(f)) {
+      currentOutSet.put(n.getID(), Sets.<AssignStmt> newHashSet(UNDEF));
     }
-    
-    public void caseFunction(Function f)
-    {
-    	currentOutSet = newInitialFlow();
-    	currentInSet = currentOutSet;
-        for (Name n: NodeFinder.find(f, Name.class)){
-        	Set<AssignStmt> s = new HashSet<AssignStmt>();
-        	s.add(UNDEF);
-        	currentOutSet.put(n.getID(), s);
-        }
-        for (Name inputArg: f.getInputParamList()){
-        	Set<AssignStmt> s = new HashSet<AssignStmt>();
-        	s.add(PARAM);
-        	currentOutSet.put(inputArg.getID(), s);
-        }
-        caseASTNode(f.getStmts());
-        outFlowSets.put(f, currentOutSet);
+    caseASTNode(f.getStmts());
+    outFlowSets.put(f, currentOutSet);
+  }
+
+  @Override
+  public HashMapFlowMap<String, Set<AssignStmt>> newInitialFlow() {
+    return copy(startMap);
+  }
+
+  @Override
+  public void caseAssignStmt(AssignStmt node) {
+    inFlowSets.put(node, currentInSet);
+    currentOutSet = copy(currentInSet);
+    for (String n : nameCollector.getNames(node)) {
+      currentOutSet.put(n, Sets.<AssignStmt> newHashSet(node));
     }
+    outFlowSets.put(node, currentOutSet);
+  }
 
-
-    public void caseScript(Script f)
-    {
-    	currentOutSet = newInitialFlow();
-    	currentInSet = currentOutSet;
-        for (Name n: NodeFinder.find(f, Name.class)){
-        	Set<AssignStmt> s = new HashSet<AssignStmt>();
-        	s.add(UNDEF);
-        	currentOutSet.put(n.getID(), s);
-        }
-        caseASTNode(f.getStmts());
-        outFlowSets.put(f, currentOutSet);
-    }
-
-    
-    
-    public HashMapFlowMap<String,Set<AssignStmt>> newInitialFlow()
-    {
-        return copy(startMap);
-    }
-
-    public void caseAssignStmt( AssignStmt node )
-    {
-        inFlowSets.put( node, currentInSet );
-        currentOutSet = copy(currentInSet);
-        Set<String> defVars = nameCollector.getNames( node );
-
-        for( String n : defVars ){
-            Set<AssignStmt> newDefSite = new HashSet<AssignStmt>();
-            newDefSite.add( node );
-            currentOutSet.put( n, newDefSite );
-        }
-        outFlowSets.put(node, currentOutSet);
-    }
-
-    public void caseStmt( Stmt node )
-    {
-        inFlowSets.put( node, currentInSet );
-        currentOutSet = currentInSet;
-        outFlowSets.put( node, currentOutSet );
-    }
-
+  @Override
+  public void caseStmt(Stmt node) {
+    inFlowSets.put(node, currentInSet);
+    currentOutSet = currentInSet;
+    outFlowSets.put(node, currentOutSet);
+  }
 }
