7b55c0f1d30415d527d743ff4940fc3f4bdbf5a9
Ismail Badawi <isbadawi@gmail.com>
2013-02-06 22:55:28 -0500

    Remove types.ast, and all references to it.
    It's obsolete, was part of the Annotations extension.
    
    Migrated from Sable/mclab@aa167eb6adfae597f0c8f9a15a46dc5ee9349f37

--- languages/Natlab/src/analysis/natlab/NatlabForwardingNodeCaseHandler.java
+++ languages/Natlab/src/analysis/natlab/NatlabForwardingNodeCaseHandler.java
@@ -35,21 +35,15 @@ public class NatlabForwardingNodeCaseHandler implements NodeCaseHandler {
     
     public void caseASTNode(ASTNode node) { callback.caseASTNode(node); }
     public void caseAndExpr(AndExpr node) { callback.caseAndExpr(node); }
-    public void caseArgTupleType(ArgTupleType node) { callback.caseArgTupleType(node); }
     public void caseArrayTransposeExpr(ArrayTransposeExpr node) { callback.caseArrayTransposeExpr(node); }
-    public void caseArrowType(ArrowType node) { callback.caseArrowType(node); }
     public void caseAssignStmt(AssignStmt node) { callback.caseAssignStmt(node); }
     public void caseAttribute(Attribute node) { callback.caseAttribute(node); }
-    public void caseBaseType(BaseType node) { callback.caseBaseType(node); }
     public void caseBinaryExpr(BinaryExpr node) { callback.caseBinaryExpr(node); }
     public void caseBody(Body node) { callback.caseBody(node); }
-    public void caseBoolType(BoolType node) { callback.caseBoolType(node); }
     public void caseBreakStmt(BreakStmt node) { callback.caseBreakStmt(node); }
     public void caseCSLExpr(CSLExpr node) { callback.caseCSLExpr(node); }
     public void caseCellArrayExpr(CellArrayExpr node) { callback.caseCellArrayExpr(node); }
-    public void caseCellArrayType(CellArrayType node) { callback.caseCellArrayType(node); }
     public void caseCellIndexExpr(CellIndexExpr node) { callback.caseCellIndexExpr(node); }
-    public void caseCharType(CharType node) { callback.caseCharType(node); }
     public void caseCheckScalarStmt(CheckScalarStmt node) { callback.caseCheckScalarStmt(node); }
     public void caseClassBody(ClassBody node) { callback.caseClassBody(node); }
     public void caseClassDef(ClassDef node) { callback.caseClassDef(node); }
@@ -72,12 +66,7 @@ public class NatlabForwardingNodeCaseHandler implements NodeCaseHandler {
     public void caseEvent(Event node) { callback.caseEvent(node); }
     public void caseExpr(Expr node) { callback.caseExpr(node); }
     public void caseExprStmt(ExprStmt node) { callback.caseExprStmt(node); }
-    public void caseExtents(Extents node) { callback.caseExtents(node); }
     public void caseFPLiteralExpr(FPLiteralExpr node) { callback.caseFPLiteralExpr(node); }
-    public void caseFieldEntry(FieldEntry node) { callback.caseFieldEntry(node); }
-    public void caseFloat32(Float32 node) { callback.caseFloat32(node); }
-    public void caseFloat64(Float64 node) { callback.caseFloat64(node); }
-    public void caseFloatType(FloatType node) { callback.caseFloatType(node); }
     public void caseForStmt(ForStmt node) { callback.caseForStmt(node); }
     public void caseFunction(Function node) { callback.caseFunction(node); }
     public void caseFunctionHandleExpr(FunctionHandleExpr node) { callback.caseFunctionHandleExpr(node); }
@@ -87,20 +76,10 @@ public class NatlabForwardingNodeCaseHandler implements NodeCaseHandler {
     public void caseGEExpr(GEExpr node) { callback.caseGEExpr(node); }
     public void caseGTExpr(GTExpr node) { callback.caseGTExpr(node); }
     public void caseGlobalStmt(GlobalStmt node) { callback.caseGlobalStmt(node); }
-    public void caseHandleType(HandleType node) { callback.caseHandleType(node); }
     public void caseHelpComment(HelpComment node) { callback.caseHelpComment(node); }
     public void caseIfBlock(IfBlock node) { callback.caseIfBlock(node); }
     public void caseIfStmt(IfStmt node) { callback.caseIfStmt(node); }
-    public void caseInt16(Int16 node) { callback.caseInt16(node); }
-    public void caseInt32(Int32 node) { callback.caseInt32(node); }
-    public void caseInt64(Int64 node) { callback.caseInt64(node); }
-    public void caseInt8(Int8 node) { callback.caseInt8(node); }
     public void caseIntLiteralExpr(IntLiteralExpr node) { callback.caseIntLiteralExpr(node); }
-    public void caseIntType(IntType node) { callback.caseIntType(node); }
-    public void caseIntersectionType(IntersectionType node) { callback.caseIntersectionType(node); }
-    public void caseKnownExtents(KnownExtents node) { callback.caseKnownExtents(node); }
-    public void caseKnownExtentsShape(KnownExtentsShape node) { callback.caseKnownExtentsShape(node); }
-    public void caseKnownRank(KnownRank node) { callback.caseKnownRank(node); }
     public void caseLEExpr(LEExpr node) { callback.caseLEExpr(node); }
     public void caseLTExpr(LTExpr node) { callback.caseLTExpr(node); }
     public void caseLValueExpr(LValueExpr node) { callback.caseLValueExpr(node); }
@@ -113,7 +92,6 @@ public class NatlabForwardingNodeCaseHandler implements NodeCaseHandler {
     public void caseMTimesExpr(MTimesExpr node) { callback.caseMTimesExpr(node); }
     public void caseMTransposeExpr(MTransposeExpr node) { callback.caseMTransposeExpr(node); }
     public void caseMatrixExpr(MatrixExpr node) { callback.caseMatrixExpr(node); }
-    public void caseMatrixType(MatrixType node) { callback.caseMatrixType(node); }
     public void caseMethods(Methods node) { callback.caseMethods(node); }
     public void caseMinusExpr(MinusExpr node) { callback.caseMinusExpr(node); }
     public void caseMultiLineHelpComment(MultiLineHelpComment node) { callback.caseMultiLineHelpComment(node); }
@@ -126,39 +104,28 @@ public class NatlabForwardingNodeCaseHandler implements NodeCaseHandler {
     public void caseParameterizedExpr(ParameterizedExpr node) { callback.caseParameterizedExpr(node); }
     public void casePersistentStmt(PersistentStmt node) { callback.casePersistentStmt(node); }
     public void casePlusExpr(PlusExpr node) { callback.casePlusExpr(node); }
-    public void casePrimitiveType(PrimitiveType node) { callback.casePrimitiveType(node); }
     public void caseProgram(Program node) { callback.caseProgram(node); }
     public void caseProperties(Properties node) { callback.caseProperties(node); }
     public void caseProperty(Property node) { callback.caseProperty(node); }
     public void casePropertyAccess(PropertyAccess node) { callback.casePropertyAccess(node); }
     public void caseRangeExpr(RangeExpr node) { callback.caseRangeExpr(node); }
     public void caseRangeForStmt(ForStmt node) { callback.caseForStmt(node); }
-    public void caseRank(Rank node) { callback.caseRank(node); }
     public void caseReturnStmt(ReturnStmt node) { callback.caseReturnStmt(node); }
     public void caseRow(Row node) { callback.caseRow(node); }
     public void caseScript(Script node) { callback.caseScript(node); }
-    public void caseShape(Shape node) { callback.caseShape(node); }
     public void caseShellCommandStmt(ShellCommandStmt node) { callback.caseShellCommandStmt(node); }
     public void caseShortCircuitAndExpr(ShortCircuitAndExpr node) { callback.caseShortCircuitAndExpr(node); }
     public void caseShortCircuitOrExpr(ShortCircuitOrExpr node) { callback.caseShortCircuitOrExpr(node); }
     public void caseSignature(Signature node) { callback.caseSignature(node); }
     public void caseStmt(Stmt node) { callback.caseStmt(node); }
     public void caseStringLiteralExpr(StringLiteralExpr node) { callback.caseStringLiteralExpr(node); }
-    public void caseStructType(StructType node) { callback.caseStructType(node); }
     public void caseSuperClass(SuperClass node) { callback.caseSuperClass(node); }
     public void caseSuperClassMethodExpr(SuperClassMethodExpr node) { callback.caseSuperClassMethodExpr(node); }
     public void caseSwitchCaseBlock(SwitchCaseBlock node) { callback.caseSwitchCaseBlock(node); }
     public void caseSwitchStmt(SwitchStmt node) { callback.caseSwitchStmt(node); }
     public void caseTryStmt(TryStmt node) { callback.caseTryStmt(node); }
-    public void caseTupleType(TupleType node) { callback.caseTupleType(node); }
-    public void caseType(Type node) { callback.caseType(node); }
-    public void caseTypeVarType(TypeVarType node) { callback.caseTypeVarType(node); }
     public void caseUMinusExpr(UMinusExpr node) { callback.caseUMinusExpr(node); }
     public void caseUPlusExpr(UPlusExpr node) { callback.caseUPlusExpr(node); }
     public void caseUnaryExpr(UnaryExpr node) { callback.caseUnaryExpr(node); }
-    public void caseUnionType(UnionType node) { callback.caseUnionType(node); }
-    public void caseUnitType(UnitType node) { callback.caseUnitType(node); }
-    public void caseUnknownType(UnknownType node) { callback.caseUnknownType(node); }
-    public void caseVarArgType(VarArgType node) { callback.caseVarArgType(node); }
     public void caseWhileStmt(WhileStmt node) { callback.caseWhileStmt(node); }
 }
