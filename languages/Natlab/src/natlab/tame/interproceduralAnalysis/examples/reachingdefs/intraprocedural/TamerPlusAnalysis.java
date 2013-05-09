744221a7d1cec79bac6664343fb101b780986cf5
Pepe <aminesahibi@gmail.com>
2013-05-09 15:31:42 -0400

    Deleted old tamer plus package
    Migrated from Sable/mclab@d82b50da021fb63f532dad0a18191fe2dee9b986

--- languages/Natlab/src/natlab/tame/interproceduralAnalysis/examples/reachingdefs/intraprocedural/TamerPlusAnalysis.java
+++
@@ -1,6 +0,0 @@
-package natlab.tame.interproceduralAnalysis.examples.reachingdefs.intraprocedural;
-
-public interface TamerPlusAnalysis
-{
-    public void analyze(AnalysisEngine engine);
-}
--- languages/Natlab/src/natlab/tame/interproceduralAnalysis/examples/reachingdefs/intraprocedural/TamerPlusTransformation.java
+++
@@ -1,9 +0,0 @@
-package natlab.tame.interproceduralAnalysis.examples.reachingdefs.intraprocedural;
-
-import ast.ASTNode;
-
-public interface TamerPlusTransformation
-{
-    public void transform(TransformationEngine engine);
-    public ASTNode<?> getTransformedTree();
-}
