777ef55d1fea78a98e9e06e4383472f2daa6e5ce
Ismail Badawi <ismail@badawi.io>
2014-06-06 19:59:12 -0400

    Add support for multi-file refactorings.
    This is used by mclab-ide-support.
    
    Migrated from Sable/mclab@dc99f1a24956b0d447d810898f47153318727834

--- languages/Natlab/src/mclint/refactoring/RefactoringContext.java
+++ languages/Natlab/src/mclint/refactoring/RefactoringContext.java
@@ -1,11 +1,17 @@
 package mclint.refactoring;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import mclint.MatlabProgram;
 import mclint.Project;
 import mclint.transform.Transformer;
 import ast.ASTNode;
 
 public class RefactoringContext {
+  private Set<MatlabProgram> modifiedPrograms = new HashSet<>();
+
   public static enum Transformations {
     BASIC,
     LAYOUT_PRESERVING
@@ -31,15 +37,23 @@ public class RefactoringContext {
     return project;
   }
 
+  public Transformer getTransformer(ASTNode<?> node) {
+    return getTransformer(node.getMatlabProgram());
+  }
+
   public Transformer getTransformer(MatlabProgram program) {
+    // Assuming that if you get a transformer then you're modifying the program.
+    // This is simpler than hooking into the Transformer somehow.
+    modifiedPrograms.add(program);
+
     if (transformations == Transformations.BASIC) {
       return program.getBasicTransformer();
     } else {
       return program.getLayoutPreservingTransformer();
     }
   }
-  
-  public Transformer getTransformer(ASTNode<?> node) {
-    return getTransformer(node.getMatlabProgram());
+
+  public Set<MatlabProgram> getModifiedPrograms() {
+    return Collections.unmodifiableSet(modifiedPrograms);
   }
 }
